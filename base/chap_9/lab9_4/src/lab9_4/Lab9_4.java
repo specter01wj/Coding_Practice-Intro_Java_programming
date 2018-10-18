a3cc6530caf02d860f0213a9a9abd5fc80c97339
specter01wj <specter01wj@gmail.com>
2018-10-18 12:13:34 -0400

    10/17/2018 v1

--- base/chap_9/lab9_4/src/lab9_4/Lab9_4.java
+++ base/chap_9/lab9_4/src/lab9_4/Lab9_4.java
@@ -5,6 +5,7 @@
  */
 package lab9_4;
 
+import java.util.Scanner;
 /**
  *
  * @author james.wang
@@ -16,6 +17,37 @@ public class Lab9_4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = input.nextLine();
+        
+        System.out.println("Palidrome check: " + s + " is? " + isPalindrome(s));
+    }
+    
+    public static boolean isPalindrome(String s) {
+        String s1 = filter(s);
+        String s2 = reverse(s1);
+        
+        return s2.equals(s1);
+    }
+    
+    public static String filter(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(Character.isLetterOrDigit(s.charAt(i))) {
+                sb.append(s.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+    
+    public static String reverse(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        
+        return sb.toString();
     }
     
 }
