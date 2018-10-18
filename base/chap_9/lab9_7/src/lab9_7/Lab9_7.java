029fdca1100603b49b44d62321f00139844abaee
specter01wj <specter01wj@gmail.com>
2018-10-18 17:59:00 -0400

    10/17/2018 v1

---
+++ base/chap_9/lab9_7/src/lab9_7/Lab9_7.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9_7;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab9_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        java.io.File file = new java.io.File("m1.txt");
+        if(file.exists()) {
+            System.out.println("File already exists!");
+            System.exit(0);
+        }
+        
+        java.io.PrintWriter output = new java.io.PrintWriter(file);
+        
+        output.print("James Wang");
+        output.close();
+    }
+    
+}
