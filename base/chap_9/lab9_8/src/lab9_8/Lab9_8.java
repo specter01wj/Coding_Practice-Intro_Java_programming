55bb9096b221e9cdff3f5c342ddb422c1d5b0be9
specter01wj <specter01wj@gmail.com>
2018-10-20 18:33:11 -0400

    10/17/2018 v1

---
+++ base/chap_9/lab9_8/src/lab9_8/Lab9_8.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9_8;
+
+import java.util.Scanner;
+/**
+ *
+ * @author james.wang
+ */
+public class Lab9_8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        java.io.File file = new java.io.File("scores.txt");
+        Scanner input = new Scanner(file);
+        
+        while(input.hasNext()) {
+            String firstName = input.next();
+            String mi = input.next();
+            String lastName = input.next();
+            int score = input.nextInt();
+            System.out.println("Output: " + firstName + " " + mi + " " + lastName + " " + score);
+        }
+        
+        input.close();
+    }
+    
+}
