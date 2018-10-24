f1d0c3bb6fd1969371941a6fb1dc1168d98842f8
specter01wj <specter01wj@gmail.com>
2018-10-24 14:13:11 -0400

    10/17/2018 v1

--- base/chap_10/lab10_3/src/lab10_3/BMI.java
+++ base/chap_10/lab10_3/src/lab10_3/BMI.java
@@ -10,5 +10,38 @@ package lab10_3;
  * @author james.wang
  */
 public class BMI {
+    private String name; 
+    private int age; 
+    private double weight, height;
+    public static final double KILO_PER_POUND = 0.454, METER_PER_ICH = 0.0254;
+
+    public BMI(String name, int age, double weight, double height) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+    
+    public BMI(String name, double weight, double height) {
+        this(name, 25, weight, height);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    
+    
     
 }
