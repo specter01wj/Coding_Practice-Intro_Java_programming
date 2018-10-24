2426d7fc55efa872fc1fb4a8e2d7961e4eba6439
specter01wj <specter01wj@gmail.com>
2018-10-24 18:11:03 -0400

    10/17/2018 v1

--- base/chap_10/lab10_5/src/lab10_5/Lab10_5.java
+++ base/chap_10/lab10_5/src/lab10_5/Lab10_5.java
@@ -16,6 +16,28 @@ public class Lab10_5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Course course1 = new Course("FPGA");
+        Course course2 = new Course("DSP");
+        
+        course1.addStudent("James Wang");
+        course1.addStudent("David Ken");
+        course1.addStudent("Sam Joren");
+        
+        course2.addStudent("Jin Wang");
+        course2.addStudent("King Steve");
+        
+        System.out.println("Num of students in C1: " + course1.getNumOfStud());
+        String[] students1 = course1.getStudents();
+        for(int i = 0; i < course1.getNumOfStud(); i++) {
+            System.out.println("Stud: " + students1[i]);
+        }
+        
+        System.out.println("Num of students in C2: " + course2.getNumOfStud());
+        String[] students2 = course2.getStudents();
+        for(int i = 0; i < course2.getNumOfStud(); i++) {
+            System.out.println("Stud: " + students2[i]);
+        }
     }
     
 }
