7950ccef232246406e6e8320eec2c3797ad62b06
specter01wj <specter01wj@gmail.com>
2018-10-25 17:14:07 -0400

    10/17/2018 v1

--- base/chap_10/lab10_7/src/lab10_7/Lab10_7.java
+++ base/chap_10/lab10_7/src/lab10_7/Lab10_7.java
@@ -23,10 +23,13 @@ public class Lab10_7 {
             stack.push(i);
         }
         
+        System.out.println("Peek: " + stack.peek());
+        System.out.println("size: " + stack.getSize());
+        
         while(!stack.empty()) {
             System.out.println("Pop out: " + stack.pop());
         }
-        
+        System.out.println("size after: " + stack.getSize());
     }
     
 }
