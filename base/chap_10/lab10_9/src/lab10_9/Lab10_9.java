7539d7366ef1b166c4d9bf9f5e3dd04166ac53c3
specter01wj <specter01wj@gmail.com>
2018-10-26 12:34:12 -0400

    10/17/2018 v1

--- base/chap_10/lab10_9/src/lab10_9/Lab10_9.java
+++ base/chap_10/lab10_9/src/lab10_9/Lab10_9.java
@@ -5,6 +5,7 @@
  */
 package lab10_9;
 
+import java.util.*;
 /**
  *
  * @author james.wang
@@ -16,6 +17,28 @@ public class Lab10_9 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int date = 0, answer;
+        
+        Scanner input = new Scanner(System.in);
+        
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Is your DOB in Set - " + (i+1) + " ?");
+            for(int j = 0; j < 4; j++) {
+                for(int k = 0; k < 4; k++) {
+                    System.out.print(GuessDate.getValue(i, j, k) + " ");
+                }
+                System.out.println();
+            }
+            
+            System.out.println("Enter 0 for No, 1 for Yes:");
+            answer = input.nextInt();
+            
+            if(answer == 1) {
+                date += GuessDate.getValue(i, 0, 0);
+            }
+        }
+        
+        System.out.println("Your DOA is: " + date);
     }
     
 }
