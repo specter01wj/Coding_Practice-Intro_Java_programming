d96984916423ada115588af04e4b171977005568
specter01wj <specter01wj@gmail.com>
2018-10-28 20:34:27 -0400

    10/28/2018 v1

--- base/chap_11/lab11_1/src/lab11_1/Circle4.java
+++ base/chap_11/lab11_1/src/lab11_1/Circle4.java
@@ -26,5 +26,30 @@ public class Circle4 extends GeometricObject1 {
         setColor(color);
         setFilled(filled);
     }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+    
+    public double getDiameter() {
+        return radius * 2;
+    }
+    
+    public double getPerimeter() {
+        return radius * 2 * Math.PI;
+    }
+    
+    public void printCircle() {
+        System.out.println("The circle is created at: " + getDateCreated() + 
+                ", radius is: " + radius);
+    }
     
 }
