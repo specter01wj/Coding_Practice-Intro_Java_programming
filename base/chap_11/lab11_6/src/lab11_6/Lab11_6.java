1eed253a01feb4cd5e54d9caea86d78ba198d054
specter01wj <specter01wj@gmail.com>
2018-10-29 13:11:05 -0400

    10/17/2018 v1

--- base/chap_11/lab11_6/src/lab11_6/Lab11_6.java
+++ base/chap_11/lab11_6/src/lab11_6/Lab11_6.java
@@ -17,7 +17,9 @@ public class Lab11_6 {
     public static void main(String[] args) {
         // TODO code application logic here
         m(new GraduateStudent());
-        
+        m(new Student());
+        m(new Person());
+        m(new Object());
     }
     
     public static void m(Object x) {
