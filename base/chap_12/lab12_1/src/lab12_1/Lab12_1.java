cc7587837ec37f4afb1a34bce1b160a2369706c0
specter01wj <specter01wj@gmail.com>
2018-10-30 12:51:35 -0400

    10/17/2018 v1

---
+++ base/chap_12/lab12_1/src/lab12_1/Lab12_1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab12_1;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author james.wang
+ */
+public class Lab12_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new JFrame("MyFrame");
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    
+}
