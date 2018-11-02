885970a95af223e1638a013877043a0b3529bbd5
specter01wj <specter01wj@gmail.com>
2018-11-02 18:01:40 -0400

    10/17/2018 v1

---
+++ base/chap_13/lab13_10/src/lab13_10/Lab13_10.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab13_10;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab13_10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.io.PrintWriter output = null;
+        
+        try {
+            output = java.io.PrintWriter("text.txt");
+            output.println("James Wang!!! hello");
+        }
+        catch(java.io.IOException ex) {
+            
+        }
+        
+    }
+    
+}
