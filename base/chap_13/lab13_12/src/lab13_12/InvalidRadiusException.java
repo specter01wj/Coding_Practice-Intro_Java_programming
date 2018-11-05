40d4731d58fb0076dee0012ffdb5fe760a0afd6f
specter01wj <specter01wj@gmail.com>
2018-11-04 21:59:45 -0500

    11/04/2018 v1

---
+++ base/chap_13/lab13_12/src/lab13_12/InvalidRadiusException.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab13_12;
+
+/**
+ *
+ * @author jinwang
+ */
+public class InvalidRadiusException extends Exception {
+    
+    private double radius;
+    
+    public InvalidRadiusException(double radius) {
+        super("Invalid radius " + radius);
+        this.radius = radius;
+    }
+    
+    public double getRadius() {
+        return radius;
+    }
+    
+}
---
+++ base/chap_13/lab13_12/src/lab13_12/Lab13_12.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab13_12;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Lab13_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        InvalidRadiusException invalid1 = new InvalidRadiusException(10);
+        //invalid1(20);
+        
+    }
+    
+}
