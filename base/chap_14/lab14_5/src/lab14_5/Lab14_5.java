f8940d6fb0d55cd85217be8b9611eeda326fe523
specter01wj <specter01wj@gmail.com>
2018-11-07 17:25:50 -0500

    10/17/2018 v1

--- base/chap_14/lab14_5/src/lab14_5/Lab14_5.java
+++ base/chap_14/lab14_5/src/lab14_5/Lab14_5.java
@@ -31,6 +31,17 @@ public class Lab14_5 {
                 dayNameOfWeek(calendar1.get(Calendar.DAY_OF_WEEK)));
     }
     
-    
+    public static String dayNameOfWeek(int dayOfWeek) {
+        switch(dayOfWeek) {
+            case 1: return "Sunday";
+            case 2: return "Monday";
+            case 3: return "Tuesday";
+            case 4: return "Wednesday";
+            case 5: return "Thursday";
+            case 6: return "Friday";
+            case 7: return "Saturday";
+            default: return null;
+        }
+    }
     
 }
