6b9c5404be77b7b6e8a1f59eed39da3edfc075ea
specter01wj <specter01wj@gmail.com>
2018-11-08 12:45:21 -0500

    10/17/2018 v1

---
+++ base/chap_14/lab14_6/src/lab14_6/Edible.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab14_6;
+
+/**
+ *
+ * @author james.wang
+ */
+public interface Edible {
+ 
+    public abstract String howToEat();
+    
+}
