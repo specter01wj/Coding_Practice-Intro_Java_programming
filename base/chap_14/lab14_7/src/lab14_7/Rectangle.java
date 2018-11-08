4fd9846caf6ea8c15b348b821ea5e6c0c6f0d680
specter01wj <specter01wj@gmail.com>
2018-11-08 18:16:43 -0500

    11/08/2018 v1

---
+++ base/chap_14/lab14_7/src/lab14_7/Rectangle.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab14_7;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Rectangle extends GeometricObject {
+    
+        private double width, height;
+    
+    public Rectangle() {
+        
+    }
+    
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    public Rectangle(double width, double height, String color, boolean filled) {
+        this.width = width;
+        this.height = height;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    
+    public double getArea() {
+        return width * height;
+    }
+    
+    public double getDiameter() {
+        return (width + height) * 2;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+}
