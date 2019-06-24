553f7701e59a364c43bcb66c4313a6c31c8dd53d
specter01wj <specter01wj@gmail.com>
2019-06-24 18:18:11 -0400

    06/24/2019 v1

---
+++ mysol/chap_14/lab14_12/src/lab14_12/Lab14_12.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab14_12;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab14_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Rational r1 = new Rational(4, 2);
+        Rational r2 = new Rational(2, 3);
+        
+        System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
+        System.out.println(r1 + " - " + r2 + " = " + r1.subtract(r2));
+        System.out.println(r1 + " * " + r2 + " = " + r1.multiply(r2));
+        System.out.println(r1 + " / " + r2 + " = " + r1.divide(r2));
+        System.out.println(r2 + " is " + r2.doubleValue());
+    }
+    
+}
