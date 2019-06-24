caf68a068551e4b09fbe1c3f6286a433e1045b77
specter01wj <specter01wj@gmail.com>
2019-06-24 18:22:55 -0400

    06/24/2019 v1

--- mysol/chap_14/lab14_12/src/lab14_12/Lab14_12.java
+++ mysol/chap_14/lab14_12/src/lab14_12/Lab14_12.java
@@ -19,11 +19,15 @@ public class Lab14_12 {
         Rational r1 = new Rational(4, 2);
         Rational r2 = new Rational(2, 3);
         
+        Rational r3 = new Rational(1, 123456789);
+        
         System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
         System.out.println(r1 + " - " + r2 + " = " + r1.subtract(r2));
         System.out.println(r1 + " * " + r2 + " = " + r1.multiply(r2));
         System.out.println(r1 + " / " + r2 + " = " + r1.divide(r2));
         System.out.println(r2 + " is " + r2.doubleValue());
+        
+        System.out.println("r1 * r2 * r3 is: " + r1.multiply(r2.multiply(r3)));
     }
     
 }
