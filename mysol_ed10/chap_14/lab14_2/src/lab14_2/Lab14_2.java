81819f66e1e408b29e7ae468a96e3b3418f2ab2e
specter01wj <specter01wj@gmail.com>
2019-06-28 18:00:07 -0400

    06/28/2019 v1

--- mysol_ed10/chap_14/lab14_2/src/lab14_2/Lab14_2.java
+++ mysol_ed10/chap_14/lab14_2/src/lab14_2/Lab14_2.java
@@ -5,17 +5,34 @@
  */
 package lab14_2;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 /**
  *
  * @author james.wang
  */
-public class Lab14_2 {
+public class Lab14_2 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        Scene scene = new Scene(new Button("OK"), 200, 250);
+        primaryStage.setTitle("MyJavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        Stage stage = new Stage();
+        stage.setTitle("Second Stage");
+        stage.setScene(new Scene(new Button("New Stage"), 100, 100));
+        stage.show();
+    }
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
     
 }
