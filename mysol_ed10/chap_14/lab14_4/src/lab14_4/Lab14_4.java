42947fffd0ab27c3449583383022a427945697b6
specter01wj <specter01wj@gmail.com>
2019-07-12 17:16:06 -0400

    06/30/2019 v1

--- mysol_ed10/chap_14/lab14_4/src/lab14_4/Lab14_4.java
+++ mysol_ed10/chap_14/lab14_4/src/lab14_4/Lab14_4.java
@@ -20,14 +20,19 @@ public class Lab14_4 {
 
     @Override
     public void start(Stage primaryStage) {
-        
+        Circle circle = new Circle();
+        circle.setCenterX(100);
+        circle.setCenterY(100);
+        circle.setRadius(50);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
     }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Application.launch(args);
     }
     
 }
