df8f2d2307530bb2d00bf1dccc3147c83582f56e
specter01wj <specter01wj@gmail.com>
2019-07-25 16:45:00 -0400

    07/25/2019 v1

---
+++ mysol_ed10/chap_14/lab14_9/src/lab14_9/Lab14_9.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab14_9;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab14_9 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new HBox(10);
+        pane.setPadding(new Insets(5,5,5,5));
+        Image image = new Image("image/us.gif");
+        pane.getChildren().add(new ImageView(image));
+        
+        ImageView imageView2 = new ImageView(image);
+        imageView2.setFitHeight(100);
+        imageView2.setFitWidth(100);
+        pane.getChildren().add(imageView2);
+        
+        ImageView imageView3 = new ImageView(image);
+        imageView3.setRotate(90);
+        pane.getChildren().add(imageView3);
+
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("ShowImage");
+        primaryStage.setScene(scene);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+}
