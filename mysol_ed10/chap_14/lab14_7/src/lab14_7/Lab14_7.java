4d3c0cec2bb8a19f9a12430866095160f666771e
specter01wj <specter01wj@gmail.com>
2019-07-25 16:14:02 -0400

    07/25/2019 v1

---
+++ mysol_ed10/chap_14/lab14_7/src/lab14_7/Lab14_7.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab14_7;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab14_7 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane pane = new StackPane();
+        Button btOK = new Button("OK");
+        btOK.setStyle("-fx-border-color: blue;");
+        pane.getChildren().add(btOK);
+        
+        pane.setRotate(45);
+        pane.setStyle("-fx-border-color: red; -fx-background-color: lightgray;");
+        
+        Scene scene = new Scene(pane, 200, 250);
+        primaryStage.setTitle("NodeStyleRotationDemo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+}
