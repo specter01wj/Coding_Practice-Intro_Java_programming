632123e97818e06d8840583ded5dfb22c7605dde
specter01wj <specter01wj@gmail.com>
2019-07-25 14:25:42 -0400

    07/25/2019 v1

--- mysol_ed10/chap_14/lab14_5/src/lab14_5/Lab14_5.java
+++ mysol_ed10/chap_14/lab14_5/src/lab14_5/Lab14_5.java
@@ -5,17 +5,42 @@
  */
 package lab14_5;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
 /**
  *
  * @author james.wang
  */
-public class Lab14_5 {
+public class Lab14_5 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        
+        Circle circle = new Circle();
+        circle.centerXProperty().bind(pane.widthProperty().divide(2));
+        circle.centerYProperty().bind(pane.heightProperty().divide(2));
+        circle.setRadius(50);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
+        pane.getChildren().add(circle);
+        
+        Scene scene = new Scene(pane, 200, 200);
+        primaryStage.setTitle("ShowCircleCentered");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
