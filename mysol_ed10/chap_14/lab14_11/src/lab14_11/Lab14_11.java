a3826c223b791ef102d16397ce7589dfae11bedf
specter01wj <specter01wj@gmail.com>
2019-07-26 17:02:31 -0400

    07/26/2019 v1

--- mysol_ed10/chap_14/lab14_11/src/lab14_11/Lab14_11.java
+++ mysol_ed10/chap_14/lab14_11/src/lab14_11/Lab14_11.java
@@ -5,17 +5,35 @@
  */
 package lab14_11;
 
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author james.wang
  */
-public class Lab14_11 {
+public class Lab14_11 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        pane.setHgap(5.5);
+        pane.setVgap(5.5);
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
