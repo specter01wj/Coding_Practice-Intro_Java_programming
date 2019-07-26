8269a04ad43d973620d9f95d96263165e793d4f0
specter01wj <specter01wj@gmail.com>
2019-07-26 16:01:22 -0400

    07/25/2019 v1

--- mysol_ed10/chap_14/lab14_10/src/lab14_10/Lab14_10.java
+++ mysol_ed10/chap_14/lab14_10/src/lab14_10/Lab14_10.java
@@ -21,7 +21,22 @@ public class Lab14_10 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        FlowPane pane = new FlowPane();
+        pane.setPadding(new Insets(11, 12, 13, 14));
+        pane.setHgap(5);
+        pane.setVgap(5);
         
+        pane.getChildren().addAll(new Label("First Name:"),
+                new TextField(), new Label("MI:"));
+        TextField tfMi = new TextField();
+        tfMi.setPrefColumnCount(1);
+        pane.getChildren().addAll(tfMi, new Label("Last Name:"), 
+                new TextField());
+        
+        Scene scene = new Scene(pane, 200, 250);
+        primaryStage.setTitle("ShowFlowPane");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
     /**
