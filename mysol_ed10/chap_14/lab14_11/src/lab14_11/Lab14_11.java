f7abdde37d81816859f48f99fa758b08abe43ebe
specter01wj <specter01wj@gmail.com>
2019-07-26 17:07:36 -0400

    07/26/2019 v1

--- mysol_ed10/chap_14/lab14_11/src/lab14_11/Lab14_11.java
+++ mysol_ed10/chap_14/lab14_11/src/lab14_11/Lab14_11.java
@@ -29,6 +29,16 @@ public class Lab14_11 extends Application {
         pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
         pane.setHgap(5.5);
         pane.setVgap(5.5);
+        
+        pane.add(new Label("First Name:"), 0, 0);
+        pane.add(new TextField(), 1, 0);
+        pane.add(new Label("MI:"), 0, 1);
+        pane.add(new TextField(), 1, 1);
+        pane.add(new Label("Last Name:"), 0, 2);
+        pane.add(new TextField(), 1, 2);
+        Button btAdd = new Button("Add Name");
+        pane.add(btAdd, 1, 3);
+        GridPane.setHalignment(btAdd, HPos.RIGHT);
     }
     /**
      * @param args the command line arguments
