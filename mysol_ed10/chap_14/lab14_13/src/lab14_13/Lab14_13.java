a7e72fe9d6175638c1a80265bb8b5e1882bf90e2
specter01wj <specter01wj@gmail.com>
2019-08-08 13:17:42 -0400

    08/08/2019 v1

--- mysol_ed10/chap_14/lab14_13/src/lab14_13/Lab14_13.java
+++ mysol_ed10/chap_14/lab14_13/src/lab14_13/Lab14_13.java
@@ -21,14 +21,53 @@ import javafx.scene.image.ImageView;
  *
  * @author james.wang
  */
-public class Lab14_13 {
+public class Lab14_13 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane pane = new BorderPane();
+        
+        pane.setTop(getHBox());
+        pane.setLeft(getVBox());
+        
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("ShowHBoxVBox");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
+    private HBox getHBox() {
+        HBox hBox = new HBox(15);
+        hBox.setPadding(new Insets(15, 15, 15, 15));
+        hBox.setStyle("-fx-background-color:gold");
+        hBox.getChildren().add(new Button("EE"));
+        hBox.getChildren().add(new Button("CE"));
+        ImageView imageView = new ImageView(new Image("image/usa.gif"));
+        hBox.getChildren().add(imageView);
+        
+        return hBox;
+    }
+    
+    private VBox getVBox() {
+        VBox vBox = new VBox(15);
+        vBox.setPadding(new Insets(15, 5, 5, 5));
+        vBox.getChildren().add(new Label("Courses"));
+        
+        Label[] courses = {new Label("ECE 1301"), new Label("ECE 1302"), 
+            new Label("ECE 1303"), new Label("ECE 1304")};
+        
+        for(Label course : courses) {
+            VBox.setMargin(course, new Insets(0, 0, 0, 15));
+            vBox.getChildren().add(course);
+        }
+        
+        return vBox;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
