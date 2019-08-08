200ded3891a502da08a023e0762c80b77e9c7a60
specter01wj <specter01wj@gmail.com>
2019-08-08 11:46:57 -0400

    08/08/2019 v1

---
+++ mysol_ed10/chap_14/lab14_12/src/lab14_12/Lab14_12.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab14_12;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab14_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
