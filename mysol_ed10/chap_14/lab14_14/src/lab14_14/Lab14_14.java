0a78eca6ba2b9d372bc5563a3a862ce75d016f65
specter01wj <specter01wj@gmail.com>
2019-08-12 12:45:51 -0400

    08/12/2019 v1

--- mysol_ed10/chap_14/lab14_14/src/lab14_14/Lab14_14.java
+++ mysol_ed10/chap_14/lab14_14/src/lab14_14/Lab14_14.java
@@ -5,6 +5,17 @@
  */
 package lab14_14;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.FontPosture;
+
 /**
  *
  * @author james.wang
