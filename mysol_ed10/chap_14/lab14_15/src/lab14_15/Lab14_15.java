23e952218e7355813f0cea16c97ad4d526a746d5
specter01wj <specter01wj@gmail.com>
2019-08-13 17:07:21 -0400

    08/08/2019 v1

--- mysol_ed10/chap_14/lab14_15/src/lab14_15/Lab14_15.java
+++ mysol_ed10/chap_14/lab14_15/src/lab14_15/Lab14_15.java
@@ -37,5 +37,17 @@ public class Lab14_15 extends Application {
 class LinePane extends Pane {
     public LinePane() {
         Line line1 = new Line(10, 10, 10, 10);
+        line1.endXProperty().bind(widthProperty().subtract(10));
+        line1.endYProperty().bind(heightProperty().subtract(10));
+        line1.setStrokeWidth(5);
+        line1.setStroke(Color.GREEN);
+        getChildren().add(line1);
+        
+        Line line2 = new Line(10, 10, 10, 10);
+        line2.startXProperty().bind(widthProperty().subtract(10));
+        line2.endYProperty().bind(heightProperty().subtract(10));
+        line2.setStrokeWidth(5);
+        line2.setStroke(Color.GREEN);
+        getChildren().add(line2);
     }
 }
