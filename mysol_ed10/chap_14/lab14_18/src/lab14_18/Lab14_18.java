13778f0ea730edd36f00915211d2c968a952d388
specter01wj <specter01wj@gmail.com>
2019-08-19 16:43:46 -0400

    08/08/2019 v1

--- mysol_ed10/chap_14/lab14_18/src/lab14_18/Lab14_18.java
+++ mysol_ed10/chap_14/lab14_18/src/lab14_18/Lab14_18.java
@@ -5,17 +5,41 @@
  */
 package lab14_18;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.text.Text;
+
 /**
  *
  * @author james.wang
  */
-public class Lab14_18 {
+public class Lab14_18 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        
+        Arc arc1 = new Arc(150, 100, 80, 80, 30, 35);
+        arc1.setFill(Color.RED);
+        arc1.setType(ArcType.ROUND);
+        pane.getChildren().add(new Text(210, 40, "arc1: round"));
+        pane.getChildren().add(arc1);
+        
+        Scene scene = new Scene(pane, 300, 200);
+        primaryStage.setTitle("ShowArc");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
