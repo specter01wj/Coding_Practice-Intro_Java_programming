96dead5aa245d9344ac85aeaf19c9433b5e6dd5f
specter01wj <specter01wj@gmail.com>
2019-08-19 13:28:28 -0400

    08/08/2019 v1

--- mysol_ed10/chap_14/lab14_17/src/lab14_17/Lab14_17.java
+++ mysol_ed10/chap_14/lab14_17/src/lab14_17/Lab14_17.java
@@ -22,7 +22,14 @@ public class Lab14_17 extends Application {
     public void start(Stage primaryStage) {
         Pane pane = new Pane();
         
-        
+        for(int i = 0; i < 16; i++) {
+            Ellipse e1 = new Ellipse(150, 100, 100, 50);
+            e1.setStroke(Color.color(Math.random(), Math.random(), Math.random()));
+//            e1.setFill(Color.WHITE);
+            e1.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+            e1.setRotate(i * 180 / 16);
+            pane.getChildren().add(e1);
+        }
         
         Scene scene = new Scene(pane, 300, 200);
         primaryStage.setTitle("ShowEllipse");
