fe53dff5f8b7d87cae49cf4aca8028a310c80dfb
specter01wj <specter01wj@gmail.com>
2019-08-19 13:03:52 -0400

    08/08/2019 v1

--- mysol_ed10/chap_14/lab14_17/src/lab14_17/Lab14_17.java
+++ mysol_ed10/chap_14/lab14_17/src/lab14_17/Lab14_17.java
@@ -5,17 +5,35 @@
  */
 package lab14_17;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Ellipse;
+
 /**
  *
  * @author james.wang
  */
-public class Lab14_17 {
+public class Lab14_17 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        
+        
+        
+        Scene scene = new Scene(pane, 300, 200);
+        primaryStage.setTitle("ShowEllipse");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
