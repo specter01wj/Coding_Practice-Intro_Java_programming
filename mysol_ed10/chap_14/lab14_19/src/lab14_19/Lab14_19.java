def6172e288bb3ad30862c5a98789d2cf77eacff
specter01wj <specter01wj@gmail.com>
2019-08-22 17:10:58 -0600

    08/08/2019 v1

--- mysol_ed10/chap_14/lab14_19/src/lab14_19/Lab14_19.java
+++ mysol_ed10/chap_14/lab14_19/src/lab14_19/Lab14_19.java
@@ -5,17 +5,36 @@
  */
 package lab14_19;
 
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Polygon;
+
 /**
  *
  * @author james.wang
  */
-public class Lab14_19 {
+public class Lab14_19 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        Polygon polygon = new Polygon();
+        pane.getChildren().add(polygon);
+        polygon.setFill(Color.WHITE);
+        polygon.setStroke(Color.BLACK);
+        ObservableList<Double> list = polygon.getPoints();
+        
+        
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
