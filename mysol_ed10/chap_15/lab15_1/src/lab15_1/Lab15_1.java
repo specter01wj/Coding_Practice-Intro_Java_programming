346ab740f6704307d6972406139a27331618fe3f
specter01wj <specter01wj@gmail.com>
2019-08-27 17:58:13 -0600

    08/27/2019 v1

---
+++ mysol_ed10/chap_15/lab15_1/src/lab15_1/Lab15_1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab15_1;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab15_1 extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+}
