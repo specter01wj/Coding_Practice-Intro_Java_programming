35ede15f1949dc2a3d25a75b5cad24c04a1e0a95
specter01wj <specter01wj@gmail.com>
2019-08-27 18:28:36 -0600

    08/27/2019 v1

--- mysol_ed10/chap_15/lab15_1/src/lab15_1/Lab15_1.java
+++ mysol_ed10/chap_15/lab15_1/src/lab15_1/Lab15_1.java
@@ -6,13 +6,13 @@
 package lab15_1;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 
 /**
  *
@@ -20,6 +20,24 @@ import javafx.event.EventHandler;
  */
 public class Lab15_1 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        HBox pane = new HBox(10);
+        pane.setAlignment(Pos.CENTER);
+        Button btOK = new Button("OK");
+        Button btCancel = new Button("Cancel");
+        
+        OKHandlerClass handler1 = new OKHandlerClass();
+        btOK.setOnAction(handler1);
+        CancelHandlerClass handler2 = new CancelHandlerClass();
+        btCancel.setOnAction(handler2);
+        pane.getChildren().addAll(btOK, btCancel);
+        
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("HandleEvent");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     /**
      * @param args the command line arguments
      */
@@ -27,4 +45,18 @@ public class Lab15_1 extends Application {
         Application.launch(args);
     }
     
+}
+
+class OKHandlerClass implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("OK button clicked!");
+    }
+}
+
+class CancelHandlerClass implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("Cancel button clicked!");
+    }
 }
