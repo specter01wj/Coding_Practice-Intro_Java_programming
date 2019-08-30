9cf941a20072628e2a236a42ef77a96149451fac
specter01wj <specter01wj@gmail.com>
2019-08-30 16:45:38 -0600

    08/30/2019 v1

--- mysol_ed10/chap_15/lab15_3/src/lab15_3/Lab15_3.java
+++ mysol_ed10/chap_15/lab15_3/src/lab15_3/Lab15_3.java
@@ -5,6 +5,19 @@
  */
 package lab15_3;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
 /**
  *
  * @author james.wang
@@ -15,7 +28,7 @@ public class Lab15_3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
