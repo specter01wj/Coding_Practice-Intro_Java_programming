3a03e43d128b5f6f75a3fd10299ad383495d864c
specter01wj <specter01wj@gmail.com>
2019-08-31 20:22:14 -0600

    08/30/2019 v1

--- mysol_ed10/chap_15/lab15_5/src/lab15_5/Lab15_5.java
+++ mysol_ed10/chap_15/lab15_5/src/lab15_5/Lab15_5.java
@@ -7,7 +7,6 @@ package lab15_5;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,8 +17,41 @@ import javafx.stage.Stage;
  *
  * @author james.wang
  */
-public class Lab15_5 {
+public class Lab15_5 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+        Button btNew = new Button("New");
+        Button btOpen = new Button("Open");
+        Button btSave = new Button("Save");
+        Button btPrint = new Button("Print");
+        hBox.getChildren().addAll(btNew, btOpen, btSave, btPrint);
+        
+        btNew.setOnAction((ActionEvent e) -> {
+            System.out.println("Process New");
+        });
+        
+        btOpen.setOnAction(e -> {
+           System.out.println("Process Open");
+        });
+        
+        btSave.setOnAction(e -> {
+            System.out.println("Process Save");
+        });
+        
+        btPrint.setOnAction(e -> {
+            System.out.println("Process Print");
+        });
+        
+        
+        Scene scene = new Scene(hBox, 300, 50);
+        primaryStage.setTitle("LambdaHandlerDemo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     /**
      * @param args the command line arguments
      */
