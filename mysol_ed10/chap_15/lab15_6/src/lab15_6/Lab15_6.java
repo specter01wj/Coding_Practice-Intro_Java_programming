0d2c23759bbc5658c194e0091d5bce132379af69
specter01wj <specter01wj@gmail.com>
2019-08-31 21:00:50 -0600

    08/30/2019 v1

--- mysol_ed10/chap_15/lab15_6/src/lab15_6/Lab15_6.java
+++ mysol_ed10/chap_15/lab15_6/src/lab15_6/Lab15_6.java
@@ -5,17 +5,38 @@
  */
 package lab15_6;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.geometry.HPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author james.wang
  */
-public class Lab15_6 {
+public class Lab15_6 extends Application {
 
+    private TextField tfAnnualInterestRate = new TextField();
+    private TextField tfNumberOfYears = new TextField();
+    private TextField tfLoanAmount = new TextField();
+    private TextField tfMonthlyPayment = new TextField();
+    private TextField tfTotalPayment = new TextField();
+    private Button btCalculate = new Button("Calculate");
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
