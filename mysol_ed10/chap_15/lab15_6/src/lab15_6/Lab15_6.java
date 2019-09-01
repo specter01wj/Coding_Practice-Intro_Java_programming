b4196a1245c393a37940ba2d5f80341b5afc3ebc
specter01wj <specter01wj@gmail.com>
2019-08-31 21:02:46 -0600

    08/30/2019 v1

--- mysol_ed10/chap_15/lab15_6/src/lab15_6/Lab15_6.java
+++ mysol_ed10/chap_15/lab15_6/src/lab15_6/Lab15_6.java
@@ -30,7 +30,40 @@ public class Lab15_6 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        
+        // Create UI
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+        gridPane.add(new Label("Annual Interest Rate:"), 0, 0);
+        gridPane.add(tfAnnualInterestRate, 1, 0);
+        gridPane.add(new Label("Number of Years:"), 0, 1);
+        gridPane.add(tfNumberOfYears, 1, 1);
+        gridPane.add(new Label("Loan Amount:"), 0, 2);
+        gridPane.add(tfLoanAmount, 1, 2);
+        gridPane.add(new Label("Monthly Payment:"), 0, 3);
+        gridPane.add(tfMonthlyPayment, 1, 3);
+        gridPane.add(new Label("Total Payment:"), 0, 4);
+        gridPane.add(tfTotalPayment, 1, 4);
+        gridPane.add(btCalculate, 1, 5);
+
+        // Set properties for UI
+        gridPane.setAlignment(Pos.CENTER);
+        tfAnnualInterestRate.setAlignment(Pos.BOTTOM_RIGHT);
+        tfNumberOfYears.setAlignment(Pos.BOTTOM_RIGHT);
+        tfLoanAmount.setAlignment(Pos.BOTTOM_RIGHT);
+        tfMonthlyPayment.setAlignment(Pos.BOTTOM_RIGHT);
+        tfTotalPayment.setAlignment(Pos.BOTTOM_RIGHT);
+        tfMonthlyPayment.setEditable(false);
+        tfTotalPayment.setEditable(false);
+        GridPane.setHalignment(btCalculate, HPos.RIGHT);
+
+        // Process events
+        btCalculate.setOnAction(e -> calculateLoanPayment());
+
+        Scene scene = new Scene(gridPane, 400, 250);
+        primaryStage.setTitle("LoanCalculator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     /**
      * @param args the command line arguments
