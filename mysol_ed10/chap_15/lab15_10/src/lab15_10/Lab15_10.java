cbbd02733d7180ece80aed9436d009b588a0ff77
specter01wj <specter01wj@gmail.com>
2019-09-02 16:04:40 -0600

    09/02/2019 v1

--- mysol_ed10/chap_15/lab15_10/src/lab15_10/Lab15_10.java
+++ mysol_ed10/chap_15/lab15_10/src/lab15_10/Lab15_10.java
@@ -20,13 +20,17 @@ public class Lab15_10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         DoubleProperty balance = new SimpleDoubleProperty();
         
         balance.addListener(new InvalidationListener() {
-            public void invalidated(Observalbe ov) {
+            public void invalidated(Observable ov) {
                 System.out.println("The new value is: " + balance.doubleValue());
             }
         });
+        
+        balance.set(4.5);
+        balance.set(15.5);
     }
     
 }
