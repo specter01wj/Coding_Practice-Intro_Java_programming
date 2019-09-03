81863252d9f8047e5095791e2370fc684fd9925d
specter01wj <specter01wj@gmail.com>
2019-09-02 18:34:19 -0600

    09/02/2019 v1

--- mysol_ed10/chap_15/lab15_11/src/lab15_11/Lab15_11.java
+++ mysol_ed10/chap_15/lab15_11/src/lab15_11/Lab15_11.java
@@ -6,13 +6,35 @@
 package lab15_11;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author james.wang
  */
-public class Lab15_11 {
+public class Lab15_11 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        ClockPane clock = new ClockPane();
+        String timeString = clock.getHour() + ":" + clock.getMinute()
+                + ":" + clock.getSecond();
+        Label lblCurrentTime = new Label(timeString);
+        
+        BorderPane pane = new BorderPane();
+        pane.setCenter(clock);
+        pane.setBottom(lblCurrentTime);
+        BorderPane.setAlignment(lblCurrentTime, Pos.TOP_CENTER);
+        
+        Scene scene = new Scene(pane, 250, 250);
+        primaryStage.setTitle("DisplayClock");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     /**
      * @param args the command line arguments
      */
