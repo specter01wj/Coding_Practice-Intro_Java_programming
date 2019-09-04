1a2a868f16c143c9f7ab9a708aff1570d46134ad
specter01wj <specter01wj@gmail.com>
2019-09-03 22:19:11 -0600

    08/30/2019 v1

--- mysol_ed10/chap_15/lab15_16/src/lab15_16/Lab15_16.java
+++ mysol_ed10/chap_15/lab15_16/src/lab15_16/Lab15_16.java
@@ -6,13 +6,46 @@
 package lab15_16;
 
 import javafx.application.Application;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
  * @author james.wang
  */
-public class Lab15_16 {
+public class Lab15_16 extends Applicaiton {
 
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        
+        Ellipse ellipse = new Ellipse(10, 10, 100, 50);
+        ellipse.setFill(Color.RED);
+        ellipse.setStroke(Color.BLACK);
+        ellipse.centerXProperty().bind(pane.widthProperty().divide(2));
+        ellipse.centerYProperty().bind(pane.heightProperty().divide(2));
+        ellipse.radiusXProperty().bind(pane.widthProperty().multiply(0.4));
+        ellipse.radiusYProperty().bind(pane.heightProperty().multiply(0.4));
+        
+        pane.getChildren().add(ellipse);
+        
+        FadeTransition ft = new FadeTransition(Duration.millis(3000), ellipse);
+        ft.setFromValue(1.0);
+        ft.setToValue(0.1);
+        ft.setCycleCount(Timeline.INDEFINITE);
+        ft.setAutoReverse(true);
+        ft.play();
+        
+        Scene scene = new Scene(pane, 200, 150);
+        primaryStage.setTitle("FadeTransitionDemo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     /**
      * @param args the command line arguments
      */
