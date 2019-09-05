9513169945b6852a2f5bb5d24f2f27b336cf4e2a
specter01wj <specter01wj@gmail.com>
2019-09-05 12:16:32 -0400

    09/05/2019 v1

--- mysol_ed10/chap_15/lab15_17_bouncingBall/src/lab15_17_bouncingball/Lab15_17_bouncingBall.java
+++ mysol_ed10/chap_15/lab15_17_bouncingBall/src/lab15_17_bouncingball/Lab15_17_bouncingBall.java
@@ -5,17 +5,49 @@
  */
 package lab15_17_bouncingball;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
 /**
  *
  * @author james.wang
  */
-public class Lab15_17_bouncingBall {
+public class Lab15_17_bouncingBall extends Application {
 
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+      BallPane ballPane = new BallPane(); // Create a ball pane
+
+      // Pause and resume animation
+      ballPane.setOnMousePressed(e -> ballPane.pause());
+      ballPane.setOnMouseReleased(e -> ballPane.play());
+
+      // Increase and decrease animation   
+      ballPane.setOnKeyPressed(e -> {
+        if (e.getCode() == KeyCode.UP) {
+          ballPane.increaseSpeed();
+        } 
+        else if (e.getCode() == KeyCode.DOWN) {
+          ballPane.decreaseSpeed();
+        }
+      });
+
+      // Create a scene and place it in the stage
+      Scene scene = new Scene(ballPane, 250, 150);
+      primaryStage.setTitle("BounceBallControl"); // Set the stage title
+      primaryStage.setScene(scene); // Place the scene in the stage
+      primaryStage.show(); // Display the stage
+
+      // Must request focus after the primary stage is displayed
+      ballPane.requestFocus();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(args);
     }
     
 }
