acc2de4d219f9657dc3ac88c682f172f778cdf42
specter01wj <specter01wj@gmail.com>
2019-09-09 02:00:26 -0400

    09/08/2019 v1

--- mysol_ed10/chap_16/lab16_3/src/lab16_3/Lab16_3.java
+++ mysol_ed10/chap_16/lab16_3/src/lab16_3/Lab16_3.java
@@ -6,13 +6,46 @@
 package lab16_3;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 /**
  *
  * @author jinwang
  */
-public class Lab16_3 {
+public class Lab16_3 extends Lab16_2 {
 
+    protected Text text = new Text(50, 50, "JavaFX Programming.");
+    
+    protected BorderPane getPane() {
+        HBox paneForButtons = new HBox(20);
+        Button btLeft = new Button("Left", new ImageView("image/left.gif"));
+        Button btRight = new Button("Right", new ImageView("image/right.gif"));
+        
+        paneForButtons.getChildren().addAll(btLeft, btRight);
+        paneForButtons.setAlignment(Pos.CENTER);
+        paneForButtons.setStyle("-fx-border-color:green");
+        
+        BorderPane pane = new BorderPane();
+        pane.setBottom(paneForButtons);
+        
+        Pane paneForText = new Pane();
+        paneForText.getChildren().add(text);
+        pane.setCenter(paneForText);
+        
+        btLeft.setOnAction(e -> text.setX(text.getX() - 10));
+        btRight.setOnAction(e -> text.setX(text.getX() + 10));
+        
+        return pane;
+    }
+    
     /**
      * @param args the command line arguments
      */
