c97dc38a03d8931bcd995b20f7c6d26f5f6e86d1
specter01wj <specter01wj@gmail.com>
2019-11-22 13:16:14 -0700

    11/22/2019 v1

---
+++ mysol_ed10/chap_16/lab16_4/src/lab16_4/Lab16_4.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab16_4;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author james.wang
+ */
+
+// RadioButtonDemo
+public class Lab16_4 extends Lab16_3 {
+
+    @Override
+    protected BorderPane getPane() {
+        BorderPane pane = super.getPane();
+        
+        return pane;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Application.launch(args);
+    }
+    
+}
