57a9e0bf4c35c54721a3d598481540ea556fc469
specter01wj <specter01wj@gmail.com>
2019-11-23 01:05:56 -0700

    11/22/2019 v1

---
+++ mysol_ed10/chap_16/lab16_6/src/lab16_6/DescriptionPane.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab16_6;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+
+/**
+ *
+ * @author james.wang
+ */
+public class DescriptionPane extends BorderPane {
+    
+}
