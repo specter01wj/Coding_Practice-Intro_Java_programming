6b8846b922ceee1331ef043fe654e6427eeef66a
specter01wj <specter01wj@gmail.com>
2019-11-23 23:54:40 -0700

    11/22/2019 v1

--- mysol_ed10/chap_16/lab16_6/src/lab16_6/Lab16_6.java
+++ mysol_ed10/chap_16/lab16_6/src/lab16_6/Lab16_6.java
@@ -30,6 +30,7 @@ public class Lab16_6 extends Application {
       
       
       
+      
       Scene scene = new Scene(descriptionPane, 450, 200);
       primaryStage.setTitle("TextAreaDemo");
       primaryStage.setScene(scene);
