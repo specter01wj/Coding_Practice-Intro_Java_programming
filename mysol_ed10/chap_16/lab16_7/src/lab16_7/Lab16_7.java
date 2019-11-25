093de76d8cf86c9917a9b03002c1cc740e675a24
specter01wj <specter01wj@gmail.com>
2019-11-24 20:04:00 -0700

    11/24/2019 v1

---
+++ mysol_ed10/chap_16/lab16_7/src/lab16_7/Lab16_7.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab16_7;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab16_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Application.launch(args);
+    }
+    
+}
