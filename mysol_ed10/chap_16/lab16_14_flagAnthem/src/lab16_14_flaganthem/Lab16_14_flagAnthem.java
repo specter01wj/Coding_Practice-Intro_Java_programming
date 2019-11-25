852209d4ff3cb4150f361d418cf2c8644ee7239f
specter01wj <specter01wj@gmail.com>
2019-11-25 13:43:56 -0700

    11/24/2019 v1

---
+++ mysol_ed10/chap_16/lab16_14_flagAnthem/src/lab16_14_flaganthem/Lab16_14_flagAnthem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab16_14_flaganthem;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab16_14_flagAnthem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
