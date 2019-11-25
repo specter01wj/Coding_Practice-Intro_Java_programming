f0bdeb065e3ace52878f7f199720f5568e454310
specter01wj <specter01wj@gmail.com>
2019-11-24 23:55:56 -0700

    11/24/2019 v1

--- mysol_ed10/chap_16/lab16_8/src/lab16_8/Lab16_8.java
+++ mysol_ed10/chap_16/lab16_8/src/lab16_8/Lab16_8.java
@@ -5,17 +5,20 @@
  */
 package lab16_8;
 
+import javafx.application.Application;
+
 /**
  *
  * @author james.wang
  */
-public class Lab16_8 {
+public class Lab16_8 extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Application.launch(args);
     }
     
 }
