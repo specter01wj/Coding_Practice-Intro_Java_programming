a9cb2480fed28fbdac82aeba292e50f8cfe0c170
specter01wj <specter01wj@gmail.com>
2019-11-25 12:19:05 -0700

    11/24/2019 v1

---
+++ mysol_ed10/chap_16/lab16_13/src/lab16_13/Lab16_13.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab16_13;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author james.wang
+ */
+
+// MediaDemo
+public class Lab16_13 extends Application {
+
+    private static final String MEDIA_URL = 
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4";
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Media media = new Media(MEDIA_URL);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        MediaView mediaView = new MediaView(mediaPlayer);
+        
+        
+        
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Application.launch(args);
+    }
+    
+}
