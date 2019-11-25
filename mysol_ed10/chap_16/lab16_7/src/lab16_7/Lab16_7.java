127e9417f1a7fa34c771d3be106ef642980b978f
specter01wj <specter01wj@gmail.com>
2019-11-24 20:17:32 -0700

    11/24/2019 v1

--- mysol_ed10/chap_16/lab16_7/src/lab16_7/Lab16_7.java
+++ mysol_ed10/chap_16/lab16_7/src/lab16_7/Lab16_7.java
@@ -40,7 +40,44 @@ public class Lab16_7 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        flagDescription[0] = "The Canadian national flag ...";
+        flagDescription[1] = "Description for China ... ";
+        flagDescription[2] = "Description for Denmark ... ";
+        flagDescription[3] = "Description for France ... ";
+        flagDescription[4] = "Description for Germany ... ";
+        flagDescription[5] = "Description for India ... ";
+        flagDescription[6] = "Description for Norway ... ";
+        flagDescription[7] = "Description for UK ... ";
+        flagDescription[8] = "Description for US ... ";
         
+        setDisplay(0);
+        
+        BorderPane pane = new BorderPane();
+      
+        BorderPane paneForComboBox = new BorderPane();
+        paneForComboBox.setLeft(new Label("Select a country: "));
+        paneForComboBox.setCenter(cbo);
+        pane.setTop(paneForComboBox);
+        cbo.setPrefWidth(400);
+        cbo.setValue("Canada");
+
+        ObservableList<String> items = 
+          FXCollections.observableArrayList(flagTitles);
+        cbo.getItems().addAll(items); 
+        pane.setCenter(descriptionPane);
+        
+        cbo.setOnAction(e -> setDisplay(items.indexOf(cbo.getValue())));
+        
+        Scene scene = new Scene(pane, 450, 170);
+        primaryStage.setTitle("ComboBoxDemo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public void setDisplay(int index) {
+        descriptionPane.setTitle(flagTitles[index]);
+        descriptionPane.setImageView(flagImage[index]);
+        descriptionPane.setDescription(flagDescription[index]);
     }
     
     /**
