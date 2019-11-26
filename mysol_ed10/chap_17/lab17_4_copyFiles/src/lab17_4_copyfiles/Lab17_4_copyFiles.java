6506f42fa738de5edfdef9d61529048be5962b39
specter01wj <specter01wj@gmail.com>
2019-11-26 16:18:50 -0700

    11/26/2019 v1

--- mysol_ed10/chap_17/lab17_4_copyFiles/src/lab17_4_copyfiles/Lab17_4_copyFiles.java
+++ mysol_ed10/chap_17/lab17_4_copyFiles/src/lab17_4_copyfiles/Lab17_4_copyFiles.java
@@ -19,13 +19,21 @@ public class Lab17_4_copyFiles {
      * @param args[0] for sourcefile 
      * @param args[1] for target file
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         if (args.length != 2) { 
             System.out.println(
               "Usage: java Copy sourceFile targetfile");
             System.exit(1);
         }
+        
+        File sourceFile = new File(args[0]);
+        if (!sourceFile.exists()) {
+           System.out.println("Source file " + args[0] 
+             + " does not exist");
+           System.exit(2);
+        }
+    
     }
     
 }
