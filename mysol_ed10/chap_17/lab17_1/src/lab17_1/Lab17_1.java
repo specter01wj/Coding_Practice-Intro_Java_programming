7cff2669899565c2b2648d44bccad02eb4b6bfd9
specter01wj <specter01wj@gmail.com>
2019-11-25 23:40:59 -0700

    11/24/2019 v1

--- mysol_ed10/chap_17/lab17_1/src/lab17_1/Lab17_1.java
+++ mysol_ed10/chap_17/lab17_1/src/lab17_1/Lab17_1.java
@@ -5,17 +5,33 @@
  */
 package lab17_1;
 
+import java.io.*;
+
 /**
  *
  * @author james.wang
  */
+
+// TestFileStream
 public class Lab17_1 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        try (FileOutputStream output = new FileOutputStream("temp.dat");) {
+            for(int i = 1; i <= 10; i++) {
+                output.write(i);
+            }
+        }
+        
+        try (FileInputStream input = new FileInputStream("temp.dat");) {
+            int value;
+            while((value = input.read()) != -1) {
+                System.out.print(value + " ");
+            }
+        }
     }
     
 }
