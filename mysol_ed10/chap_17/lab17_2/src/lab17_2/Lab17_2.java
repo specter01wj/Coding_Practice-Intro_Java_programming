3537fb2b4dace1224674228489139a8248752c73
specter01wj <specter01wj@gmail.com>
2019-11-26 13:16:57 -0700

    11/24/2019 v1

---
+++ mysol_ed10/chap_17/lab17_2/src/lab17_2/Lab17_2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab17_2;
+
+import java.io.*;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab17_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        try(DataOutputStream output = new DataOutputStream(new FileOutputStream("temp.dat"));) {
+            output.writeUTF("James");
+            output.writeDouble(95.5);
+            output.writeUTF("Kim");
+            output.writeDouble(185.5);
+            output.writeUTF("George");
+            output.writeDouble(105.25);
+        }
+        
+        try (DataInputStream input = new DataInputStream(new FileInputStream("temp.dat"));) {
+            System.out.println(input.readUTF() + " " + input.readDouble());
+            System.out.println(input.readUTF() + " " + input.readDouble());
+            System.out.println(input.readUTF() + " " + input.readDouble());
+        }
+    }
+    
+}
