1b315c8491af264541199687fc0a317a7277989f
specter01wj <specter01wj@gmail.com>
2019-11-27 10:28:02 -0700

    11/24/2019 v1

--- mysol_ed10/chap_17/lab17_6/src/lab17_6/Lab17_6.java
+++ mysol_ed10/chap_17/lab17_6/src/lab17_6/Lab17_6.java
@@ -20,6 +20,16 @@ public class Lab17_6 {
      */
     public static void main(String[] args) throws ClassNotFoundException, IOException {
         // TODO code application logic here
+        try ( // Create an input stream for file object.dat
+      ObjectInputStream input =
+        new ObjectInputStream(new FileInputStream("object.dat"));
+    ) {
+      // Read a string, double value, and object from the file
+      String name = input.readUTF();
+      double score = input.readDouble();
+      java.util.Date date = (java.util.Date)(input.readObject());
+      System.out.println(name + " " + score + " " + date);
+    }
     }
     
 }
