4c725e1f9e3491122bedd7c922d678386e99e0f4
specter01wj <specter01wj@gmail.com>
2019-11-27 10:27:07 -0700

    11/24/2019 v1

--- mysol_ed10/chap_17/lab17_6/src/lab17_6/Lab17_6.java
+++ mysol_ed10/chap_17/lab17_6/src/lab17_6/Lab17_6.java
@@ -18,7 +18,7 @@ public class Lab17_6 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
         // TODO code application logic here
     }
     
