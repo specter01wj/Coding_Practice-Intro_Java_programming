e67d811740a5309250bd1c1d06cd29e9ad603cc3
specter01wj <specter01wj@gmail.com>
2019-11-27 10:28:23 -0700

    11/24/2019 v1

--- mysol_ed10/chap_17/lab17_6/src/lab17_6/Lab17_6.java
+++ mysol_ed10/chap_17/lab17_6/src/lab17_6/Lab17_6.java
@@ -23,13 +23,13 @@ public class Lab17_6 {
         try ( // Create an input stream for file object.dat
             ObjectInputStream input =
               new ObjectInputStream(new FileInputStream("object.dat"));
-          ) {
+        ) {
             // Read a string, double value, and object from the file
             String name = input.readUTF();
             double score = input.readDouble();
             java.util.Date date = (java.util.Date)(input.readObject());
             System.out.println(name + " " + score + " " + date);
-          }
+        }
     }
     
 }
