a78530c99872d4a215aaec873f1ca0e70a0a3ba2
specter01wj <specter01wj@gmail.com>
2019-11-27 23:18:37 -0700

    11/24/2019 v1

---
+++ mysol_ed10/chap_17/lab17_8/src/lab17_8/Lab17_8.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab17_8;
+
+import java.io.*;
+
+/**
+ *
+ * @author james.wang
+ */
+
+// TestRandomAccessFile
+public class Lab17_8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
