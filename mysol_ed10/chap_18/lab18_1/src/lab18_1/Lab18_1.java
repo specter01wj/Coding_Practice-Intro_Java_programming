85966b6593a259fe84969c98045f32012a881822
specter01wj <specter01wj@gmail.com>
2019-12-03 23:28:25 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_1/src/lab18_1/Lab18_1.java
+++ mysol_ed10/chap_18/lab18_1/src/lab18_1/Lab18_1.java
@@ -20,6 +20,7 @@ public class Lab18_1 {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
 
+
         
         System.out.println("Enter a non-neg integer: ");
         int n = input.nextInt();
