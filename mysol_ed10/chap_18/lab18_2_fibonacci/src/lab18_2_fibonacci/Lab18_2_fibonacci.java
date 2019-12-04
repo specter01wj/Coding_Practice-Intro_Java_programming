576311127a486ac648720df3bce7c0cfc696e7bb
specter01wj <specter01wj@gmail.com>
2019-12-04 13:44:37 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_2_fibonacci/src/lab18_2_fibonacci/Lab18_2_fibonacci.java
+++ mysol_ed10/chap_18/lab18_2_fibonacci/src/lab18_2_fibonacci/Lab18_2_fibonacci.java
@@ -5,6 +5,8 @@
  */
 package lab18_2_fibonacci;
 
+import java.util.Scanner;
+
 /**
  *
  * @author james.wang
@@ -16,6 +18,21 @@ public class Lab18_2_fibonacci {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter index for Fibonacci: ");
+        int index = input.nextInt();
+        
+        System.out.println("Fibonacci number at index: " + index + " is: " + fib(index));
+    }
+    
+    public static long fib(long index) {
+        if(index == 0) {
+            return 0;
+        } else if(index == 1) {
+            return 1;
+        } else {
+            return fib(index - 1) + fib(index - 2);
+        }
     }
     
 }
