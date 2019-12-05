192910d1dba6f32b0a9c44f3b3334442c5a7d348
specter01wj <specter01wj@gmail.com>
2019-12-04 20:21:42 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_5/src/lab18_5/Lab18_5.java
+++ mysol_ed10/chap_18/lab18_5/src/lab18_5/Lab18_5.java
@@ -24,6 +24,18 @@ public class Lab18_5 {
     
     public static void sort(double[] list, int low, int high) {
         if(low < high) {
+            int indexOfMin = low;
+            double min = list[low];
+            
+            for(int i = low + 1; i <= high; i++) {
+                if(list[i] < min) {
+                    min = list[i];
+                    indexOfMin = i;
+                }
+            }
+            
+            list[indexOfMin] = list[low];
+            list[low] = min;
             
         }
     }
