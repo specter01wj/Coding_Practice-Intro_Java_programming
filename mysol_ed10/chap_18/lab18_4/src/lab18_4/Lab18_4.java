c9f2ccb2ede950c212076cef4c18d16dd98257ed
specter01wj <specter01wj@gmail.com>
2019-12-04 19:44:07 -0500

    11/24/2019 v1

---
+++ mysol_ed10/chap_18/lab18_4/src/lab18_4/Lab18_4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab18_4;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab18_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
