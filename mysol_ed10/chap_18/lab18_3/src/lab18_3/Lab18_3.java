cc109777f2a4f4c0aaf2476206ea2aff057fc664
specter01wj <specter01wj@gmail.com>
2019-12-04 19:17:19 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_3/src/lab18_3/Lab18_3.java
+++ mysol_ed10/chap_18/lab18_3/src/lab18_3/Lab18_3.java
@@ -18,4 +18,14 @@ public class Lab18_3 {
         // TODO code application logic here
     }
     
+    public static boolean isPalindrome(String s) {
+        if(s.length() <= 1) {
+            return true;
+        } else if(s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        } else {
+            return isPalindrome(s.substring(1, s.length() - 1));
+        }
+    }
+    
 }
