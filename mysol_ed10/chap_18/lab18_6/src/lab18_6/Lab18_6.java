ee187f73b155b3c90c8609d62f2f927b044d9dbb
specter01wj <specter01wj@gmail.com>
2019-12-06 18:20:30 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_6/src/lab18_6/Lab18_6.java
+++ mysol_ed10/chap_18/lab18_6/src/lab18_6/Lab18_6.java
@@ -27,7 +27,19 @@ public class Lab18_6 {
     }
     
     public static int recursiveBinarySearch(int[] list, int key, int low, int high) {
+        if(low > high) {
+            return -low - 1;
+        }
         
+        int mid = (low + high) / 2;
+        
+        if(key < list[mid]) {
+            return recursiveBinarySearch(list, key, low, mid - 1);
+        } else if(key == list[mid]) {
+            return mid;
+        } else {
+            return recursiveBinarySearch(list, key, mid + 1, high);
+        }
     }
     
 }
