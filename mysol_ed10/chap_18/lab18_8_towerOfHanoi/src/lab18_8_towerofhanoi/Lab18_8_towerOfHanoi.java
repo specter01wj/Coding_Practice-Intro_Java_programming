ca5d0edbc91669bf007995715a6bc6b9907448d5
specter01wj <specter01wj@gmail.com>
2019-12-07 18:04:51 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_8_towerOfHanoi/src/lab18_8_towerofhanoi/Lab18_8_towerOfHanoi.java
+++ mysol_ed10/chap_18/lab18_8_towerOfHanoi/src/lab18_8_towerofhanoi/Lab18_8_towerOfHanoi.java
@@ -31,6 +31,10 @@ public class Lab18_8_towerOfHanoi {
     public static void moveDisks(int n, char fromTower, char toTower, char auxTower) {
         if(n == 1) {
             System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower);
+        } else {
+            moveDisks(n - 1, fromTower, auxTower, toTower);
+            System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower);
+            moveDisks(n - 1, auxTower, toTower, fromTower);
         }
     }
     
