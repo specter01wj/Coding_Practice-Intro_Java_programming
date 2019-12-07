5fee33d1e0d3f6cae55c7bac8d573667a0a51df6
specter01wj <specter01wj@gmail.com>
2019-12-07 18:21:15 -0500

    11/24/2019 v1

---
+++ mysol_ed10/chap_18/lab18_9_fractals/src/lab18_9_fractals/Lab18_9_fractals.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab18_9_fractals;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab18_9_fractals {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
