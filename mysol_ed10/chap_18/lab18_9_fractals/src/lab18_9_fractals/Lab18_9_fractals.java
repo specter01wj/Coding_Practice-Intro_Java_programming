472ceae457f35381224bb29ae6b62d3dc0f22e0c
specter01wj <specter01wj@gmail.com>
2019-12-07 18:22:08 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_9_fractals/src/lab18_9_fractals/Lab18_9_fractals.java
+++ mysol_ed10/chap_18/lab18_9_fractals/src/lab18_9_fractals/Lab18_9_fractals.java
@@ -5,6 +5,19 @@
  */
 package lab18_9_fractals;
 
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+
 /**
  *
  * @author james.wang
