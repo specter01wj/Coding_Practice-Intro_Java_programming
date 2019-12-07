2789340a251434d81618eacbacac7eb5d9d48987
specter01wj <specter01wj@gmail.com>
2019-12-07 18:23:36 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_9_fractals/src/lab18_9_fractals/Lab18_9_fractals.java
+++ mysol_ed10/chap_18/lab18_9_fractals/src/lab18_9_fractals/Lab18_9_fractals.java
@@ -26,6 +26,33 @@ import javafx.stage.Stage;
 // SierpinskiTriangle
 public class Lab18_9_fractals {
 
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {       
+      SierpinskiTrianglePane trianglePane = new SierpinskiTrianglePane(); 
+      TextField tfOrder = new TextField(); 
+      tfOrder.setOnAction(
+        e -> trianglePane.setOrder(Integer.parseInt(tfOrder.getText())));
+      tfOrder.setPrefColumnCount(4);
+      tfOrder.setAlignment(Pos.BOTTOM_RIGHT);
+
+      // Pane to hold label, text field, and a button
+      HBox hBox = new HBox(10);
+      hBox.getChildren().addAll(new Label("Enter an order: "), tfOrder);
+      hBox.setAlignment(Pos.CENTER);
+
+      BorderPane borderPane = new BorderPane();
+      borderPane.setCenter(trianglePane);
+      borderPane.setBottom(hBox);
+
+      // Create a scene and place it in the stage
+      Scene scene = new Scene(borderPane, 200, 210);
+      primaryStage.setTitle("SierpinskiTriangle"); // Set the stage title
+      primaryStage.setScene(scene); // Place the scene in the stage
+      primaryStage.show(); // Display the stage
+
+      scene.widthProperty().addListener(ov -> trianglePane.paint());
+      scene.heightProperty().addListener(ov -> trianglePane.paint());
+    }
     /**
      * @param args the command line arguments
      */
