ff683796aac5f307471f77e64962ebd663659d56
specter01wj <specter01wj@gmail.com>
2019-12-07 23:05:27 -0500

    11/24/2019 v1

--- mysol_ed10/chap_18/lab18_10/src/lab18_10/Lab18_10.java
+++ mysol_ed10/chap_18/lab18_10/src/lab18_10/Lab18_10.java
@@ -5,6 +5,8 @@
  */
 package lab18_10;
 
+import java.util.Scanner;
+
 /**
  *
  * @author james.wang
@@ -16,6 +18,22 @@ public class Lab18_10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a non-neg integer: ");
+        int n = input.nextInt();
+        
+        System.out.println("Factorial of " + n + " is " + factorial(n));
+    }
+    
+    public static long factorial(int n) {
+        return factorial(n, 1);
+    }
+    
+    private static long factorial(int n, int result) {
+        if (n == 0) 
+          return result;
+        else
+          return factorial(n - 1, n * result);
     }
     
 }
