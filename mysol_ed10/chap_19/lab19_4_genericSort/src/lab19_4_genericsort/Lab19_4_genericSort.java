d1234fe26f0f85eecaa477428ac6383db2fd527a
specter01wj <specter01wj@gmail.com>
2019-12-08 22:04:04 -0500

    11/24/2019 v1

--- mysol_ed10/chap_19/lab19_4_genericSort/src/lab19_4_genericsort/Lab19_4_genericSort.java
+++ mysol_ed10/chap_19/lab19_4_genericSort/src/lab19_4_genericsort/Lab19_4_genericSort.java
@@ -38,4 +38,27 @@ public class Lab19_4_genericSort {
         printList(stringArray);
     }
     
+    public static <E extends Comparable<E>> void sort(E[] list) {
+        E currentMin;
+        int currentMinIndex;
+
+        for (int i = 0; i < list.length - 1; i++) {
+          // Find the minimum in the list[i..list.length-1]
+          currentMin = list[i];
+          currentMinIndex = i;
+
+          for (int j = i + 1; j < list.length; j++) {
+            if (currentMin.compareTo(list[j]) > 0) {
+              currentMin = list[j];
+              currentMinIndex = j;
+            }
+          }
+
+          // Swap list[i] with list[currentMinIndex] if necessary;
+          if (currentMinIndex != i) {
+            list[currentMinIndex] = list[i];
+            list[i] = currentMin;
+          }
+        }
+    }
 }
