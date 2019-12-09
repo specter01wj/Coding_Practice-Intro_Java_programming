191b1fa650f3f619ea5733b6bd97490cde084d76
specter01wj <specter01wj@gmail.com>
2019-12-08 21:27:07 -0500

    11/24/2019 v1

--- mysol_ed10/chap_19/lab19_1/src/lab19_1/Lab19_1.java
+++ mysol_ed10/chap_19/lab19_1/src/lab19_1/Lab19_1.java
@@ -9,6 +9,8 @@ package lab19_1;
  *
  * @author james.wang
  */
+
+// GenericStack
 public class Lab19_1 {
 
     /**
@@ -16,6 +18,40 @@ public class Lab19_1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GenericStack<String> stack1 = new GenericStack<>();
+        
+    }
+    
+    public static class GenericStack<E> {
+        private java.util.ArrayList<E> list = new java.util.ArrayList<>();
+        
+        public int getSize() {
+            return list.size();
+        }
+        
+        public E peek() {
+            return list.get(getSize() - 1);
+        }
+        
+        public void push(E o) {
+            list.add(o);
+        }
+        
+        public E pop() {
+            E o = list.get(getSize() - 1);
+            list.remove(getSize() - 1);
+            return o;
+        }
+        
+        public boolean isEmpty() {
+            return list.isEmpty();
+        }
+        
+        @Override
+        public String toString() {
+            return "stack: " + list.toString();
+        }
+        
     }
     
 }
