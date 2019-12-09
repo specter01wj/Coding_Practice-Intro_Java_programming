2f8795c45a4d7a667146ade5b0174887b37d22f0
specter01wj <specter01wj@gmail.com>
2019-12-08 21:50:42 -0500

    11/24/2019 v1

---
+++ mysol_ed10/chap_19/Lab19_3/src/lab19_3/Lab19_3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab19_3;
+
+/**
+ *
+ * @author james.wang
+ */
+
+// BoundedTypeDemo
+public class Lab19_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static <E extends >
+    
+}
