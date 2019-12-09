85ab77e1a58187130ec345122b02b32e32cd7ea0
specter01wj <specter01wj@gmail.com>
2019-12-08 22:02:56 -0500

    11/24/2019 v1

--- mysol_ed10/chap_19/lab19_4_genericSort/src/lab19_4_genericsort/Lab19_4_genericSort.java
+++ mysol_ed10/chap_19/lab19_4_genericSort/src/lab19_4_genericsort/Lab19_4_genericSort.java
@@ -18,27 +18,16 @@ public class Lab19_4_genericSort {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Integer[] intArray = {new Integer(2), new Integer(4),
-        new Integer(3)};
-
-        // Create a Double array
-        Double[] doubleArray = {new Double(3.4), new Double(1.3),
-          new Double(-22.1)};
-
-        // Create a Character array
-        Character[] charArray = {new Character('a'),
-          new Character('J'), new Character('r')};
-
-        // Create a String array
+        Integer[] intArray = {new Integer(2), new Integer(4), new Integer(3)};
+        Double[] doubleArray = {new Double(3.4), new Double(1.3), new Double(-22.1)}; 
+        Character[] charArray = {new Character('a'), new Character('J'), new Character('r')};
         String[] stringArray = {"Tom", "Susan", "Kim"};
-
-        // Sort the arrays
+        
         sort(intArray);
         sort(doubleArray);
         sort(charArray);
         sort(stringArray);
-
-        // Display the sorted arrays
+        
         System.out.print("Sorted Integer objects: ");
         printList(intArray);
         System.out.print("Sorted Double objects: ");
