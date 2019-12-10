1f85f6eeda88cb09180808f0d67132e589250654
specter01wj <specter01wj@gmail.com>
2019-12-09 19:42:38 -0500

    12/09/2019 v1

--- mysol_ed10/chap_19/lab19_8_anyWildCardDemo/src/lab19_8_anywildcarddemo/Lab19_8_anyWildCardDemo.java
+++ mysol_ed10/chap_19/lab19_8_anyWildCardDemo/src/lab19_8_anywildcarddemo/Lab19_8_anyWildCardDemo.java
@@ -20,12 +20,19 @@ public class Lab19_8_anyWildCardDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GenericStack<Integer> intStack = new GenericStack<>();
+        intStack.push(1);
+        intStack.push(2);
+        intStack.push(-2);
+
+        print(intStack);
     }
     
     public static void print(GenericStack<?> stack) {
         while (!stack.isEmpty()) {
           System.out.print(stack.pop() + " ");
         }
+        System.out.println("");
     }
     
 }
