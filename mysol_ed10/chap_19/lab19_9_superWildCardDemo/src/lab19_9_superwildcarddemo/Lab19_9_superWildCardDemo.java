a0a9d4108f82afd8e7d4fdcc6fb44095e32289fc
specter01wj <specter01wj@gmail.com>
2019-12-09 19:51:31 -0500

    12/09/2019 v1

---
+++ mysol_ed10/chap_19/lab19_9_superWildCardDemo/src/lab19_9_superwildcarddemo/Lab19_9_superWildCardDemo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab19_9_superwildcarddemo;
+
+/**
+ *
+ * @author james.wang
+ */
+
+// SuperWildCardDemo
+public class Lab19_9_superWildCardDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
