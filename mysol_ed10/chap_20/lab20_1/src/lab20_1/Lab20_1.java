69cb8650d35fd26fd700074d56030b0a53d824ef
specter01wj <specter01wj@gmail.com>
2019-12-11 15:01:51 -0500

    11/24/2019 v1

--- mysol_ed10/chap_20/lab20_1/src/lab20_1/Lab20_1.java
+++ mysol_ed10/chap_20/lab20_1/src/lab20_1/Lab20_1.java
@@ -26,13 +26,40 @@ public class Lab20_1 {
         arrList1.add("Dallas");
         arrList1.add("Baltimore");
         
-        System.out.println("A List of collection1: ");
+        System.out.println("A List1 of collection1: ");
         System.out.println(arrList1);
         System.out.println("Dallas? - " + arrList1.contains("Dallas"));
         
         System.out.println("Before: " + arrList1.size() + " cities.");
         arrList1.remove("Dallas");
         System.out.println("After: " + arrList1.size() + " cities.");
+        
+        System.out.println("");
+        
+        Collection<String> arrList2 = new ArrayList<>();
+        arrList2.add("Seattle"); 
+        arrList2.add("Portland"); 
+        arrList2.add("Los Angles"); 
+        arrList2.add("Boston"); 
+        arrList2.add("Baltimore"); 
+        
+        System.out.println("A List2 of collection1: ");
+        System.out.println(arrList2);
+        
+        ArrayList<String> c1 = (ArrayList<String>)(arrList1.clone());
+        c1.addAll(arrList2);
+        System.out.println("Cities in collection1 or collection2: ");
+        System.out.println(c1);
+        
+        c1 = (ArrayList<String>)(arrList1.clone());
+        c1.retainAll(arrList2);
+        System.out.print("Cities in collection1 and collection2: ");
+        System.out.println(c1);
+        
+        c1 = (ArrayList<String>)(arrList1.clone());
+        c1.removeAll(arrList2);
+        System.out.print("Cities in collection1, but not in 2: ");
+        System.out.println(c1);
     }
     
 }
