34b87af3ec446b717275bce0e11b3cd3e4c17456
specter01wj <specter01wj@gmail.com>
2019-12-11 03:38:10 -0500

    11/24/2019 v1

--- mysol_ed10/chap_19/lab19_11_genericmatrix_RationalMatrix/src/lab19_11_genericmatrix_rationalmatrix/Lab19_11_genericmatrix_RationalMatrix.java
+++ mysol_ed10/chap_19/lab19_11_genericmatrix_RationalMatrix/src/lab19_11_genericmatrix_rationalmatrix/Lab19_11_genericmatrix_RationalMatrix.java
@@ -16,6 +16,22 @@ public class Lab19_11_genericmatrix_RationalMatrix {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Rational[][] m1 = new Rational[3][3];
+        Rational[][] m2 = new Rational[3][3];
+        for (int i = 0; i < m1.length; i++) {
+          for (int j = 0; j < m1[0].length; j++) {
+            m1[i][j] = new Rational(i + 1, j + 5);
+            m2[i][j] = new Rational(i + 1, j + 6);
+          }
+        }
+        
+        RationalMatrix ratMat = new RationalMatrix();
+
+        System.out.println("\nm1 + m2 is ");
+        GenericMatrix.printResult(m1, m2, ratMat.addMatrix(m1, m2), '+');
+
+        System.out.println("\nm1 * m2 is ");
+        GenericMatrix.printResult(m1, m2, ratMat.multiplyMatrix(m1, m2), '*');
     }
     
 }
