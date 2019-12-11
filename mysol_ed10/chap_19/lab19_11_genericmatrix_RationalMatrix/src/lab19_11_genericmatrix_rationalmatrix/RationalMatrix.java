b1a0e2b9d231d66d2058a0faa0f551727de2d6bc
specter01wj <specter01wj@gmail.com>
2019-12-11 03:36:12 -0500

    11/24/2019 v1

---
+++ mysol_ed10/chap_19/lab19_11_genericmatrix_RationalMatrix/src/lab19_11_genericmatrix_rationalmatrix/RationalMatrix.java
@@ -0,0 +1,20 @@
+package lab19_11_genericmatrix_rationalmatrix;
+
+import lab19_11_genericmatrix_rationalmatrix.Rational;
+
+public class RationalMatrix extends GenericMatrix<Rational> {
+  @Override /** Add two rational numbers */
+  protected Rational add(Rational r1, Rational r2) {
+    return r1.add(r2);
+  }
+
+  @Override /** Multiply two rational numbers */
+  protected Rational multiply(Rational r1, Rational r2) {
+    return r1.multiply(r2);
+  }
+
+  @Override /** Specify zero for a Rational number */
+  protected Rational zero() {
+    return new Rational(0,1);
+  }
+}
