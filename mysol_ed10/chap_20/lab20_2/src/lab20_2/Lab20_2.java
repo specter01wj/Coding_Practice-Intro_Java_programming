4d1ff8de268672f278891a025a1766b244ce5002
specter01wj <specter01wj@gmail.com>
2019-12-12 18:51:51 -0500

    11/24/2019 v1

---
+++ mysol_ed10/chap_20/lab20_2/src/lab20_2/Lab20_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab20_2;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Lab20_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
