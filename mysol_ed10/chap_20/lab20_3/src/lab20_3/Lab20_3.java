e32ec28918905e1841d56549c146cb0ebac30a24
specter01wj <specter01wj@gmail.com>
2019-12-13 18:17:10 -0500

    12/13/2019 v1

--- mysol_ed10/chap_20/lab20_3/src/lab20_3/Lab20_3.java
+++ mysol_ed10/chap_20/lab20_3/src/lab20_3/Lab20_3.java
@@ -22,6 +22,12 @@ public class Lab20_3 {
         // TODO code application logic here
         List<Integer> arrList1 = new ArrayList<>();
         arrList1.add(1);
+        arrList1.add(2);
+        arrList1.add(3);
+        arrList1.add(1);
+        arrList1.add(4);
+        arrList1.add(0, 10);
+        arrList1.add(3, 30);
     }
     
 }
