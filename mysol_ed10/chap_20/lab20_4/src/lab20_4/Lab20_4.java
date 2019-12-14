0dee43f96efeeaace0d1c1053cfaf6dc0df26270
specter01wj <specter01wj@gmail.com>
2019-12-14 13:12:53 -0500

    12/14/2019 v1

--- mysol_ed10/chap_20/lab20_4/src/lab20_4/Lab20_4.java
+++ mysol_ed10/chap_20/lab20_4/src/lab20_4/Lab20_4.java
@@ -27,6 +27,8 @@ public class Lab20_4 {
         GeometricObject g2 = new Circle(5);
         
         GeometricObject g = max(g1, g2, new GeometricObjectComparator());
+        
+        System.out.println("The area of the larger object is " + g.getArea());
     }
     
     public static GeometricObject max(GeometricObject g1, GeometricObject g2, Comparator<GeometricObject> c) {
