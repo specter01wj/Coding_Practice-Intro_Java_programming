d4b8cdf5234f40f0c9f191668c302bf9f3d66881
specter01wj <specter01wj@gmail.com>
2019-12-15 13:30:22 -0500

    12/15/2019 v1

--- mysol_ed10/chap_20/lab20_5_multipleBounceBall/src/lab20_5_multiplebounceball/Lab20_5_multipleBounceBall.java
+++ mysol_ed10/chap_20/lab20_5_multipleBounceBall/src/lab20_5_multiplebounceball/Lab20_5_multipleBounceBall.java
@@ -30,6 +30,42 @@ import javafx.util.Duration;
 // MultipleBounceBall
 public class Lab20_5_multipleBounceBall extends Application {
 
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        MultipleBallPane ballPane = new MultipleBallPane();
+        ballPane.setStyle("-fx-border-color: yellow");
+
+        Button btAdd = new Button("+");
+        Button btSubtract = new Button("-");
+        HBox hBox = new HBox(10);
+        hBox.getChildren().addAll(btAdd, btSubtract);
+        hBox.setAlignment(Pos.CENTER);
+
+        // Add or remove a ball
+        btAdd.setOnAction(e -> ballPane.add());
+        btSubtract.setOnAction(e -> ballPane.subtract());
+
+        // Pause and resume animation
+        ballPane.setOnMousePressed(e -> ballPane.pause());
+        ballPane.setOnMouseReleased(e -> ballPane.play());
+
+        // Use a scroll bar to control animation speed
+        ScrollBar sbSpeed = new ScrollBar();
+        sbSpeed.setMax(20);
+        sbSpeed.setValue(10);
+        ballPane.rateProperty().bind(sbSpeed.valueProperty());
+
+        BorderPane pane = new BorderPane();
+        pane.setCenter(ballPane);
+        pane.setTop(sbSpeed);
+        pane.setBottom(hBox);
+
+        // Create a scene and place the pane in the stage
+        Scene scene = new Scene(pane, 250, 150);
+        primaryStage.setTitle("MultipleBounceBall"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
     /**
      * @param args the command line arguments
      */
