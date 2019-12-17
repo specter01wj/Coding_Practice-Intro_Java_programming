c6f709194d5ce58149783a8ccf113e60f180f473
specter01wj <specter01wj@gmail.com>
2019-12-17 18:37:58 -0500

    12/17/2019 v1

--- mysol_ed10/chap_21/lab21_6/src/lab21_6/Lab21_6.java
+++ mysol_ed10/chap_21/lab21_6/src/lab21_6/Lab21_6.java
@@ -20,6 +20,7 @@ public class Lab21_6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        List<Integer> list1 = new ArrayList<>();
     }
     
 }
