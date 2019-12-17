da1338caeaf460707d3a46efb28ff498c58d8a0c
specter01wj <specter01wj@gmail.com>
2019-12-17 18:42:35 -0500

    12/17/2019 v1

--- mysol_ed10/chap_21/lab21_6/src/lab21_6/Lab21_6.java
+++ mysol_ed10/chap_21/lab21_6/src/lab21_6/Lab21_6.java
@@ -37,7 +37,17 @@ public class Lab21_6 {
         long startTime = System.currentTimeMillis();
         
         for (int i = 0; i < N; i++) {
-          c.contains((int)(Math.random() * 2 * N));
+            c.contains((int)(Math.random() * 2 * N));
+        }
+
+        return System.currentTimeMillis() - startTime; 
+    }
+    
+    public static long getRemoveTime(Collection<Integer> c) {
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < N; i++) {
+            c.remove(i);
         }
 
         return System.currentTimeMillis() - startTime; 
