c92327d03acc1a55d3415e509114b3e6549f46ea
specter01wj <specter01wj@gmail.com>
2019-12-17 18:38:51 -0500

    12/17/2019 v1

--- mysol_ed10/chap_21/lab21_6/src/lab21_6/Lab21_6.java
+++ mysol_ed10/chap_21/lab21_6/src/lab21_6/Lab21_6.java
@@ -14,7 +14,9 @@ import java.util.*;
 
 // SetListPerformanceTest
 public class Lab21_6 {
-
+    
+    static final int N = 50000;
+    
     /**
      * @param args the command line arguments
      */
