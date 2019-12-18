6cfc22d688f0d872a261fd8e2479312892e1ce8e
specter01wj <specter01wj@gmail.com>
2019-12-18 17:48:37 -0500

    11/24/2019 v1

--- mysol_ed10/chap_21/lab21_9_countOccurrenceOfWords/src/lab21_9_countoccurrenceofwords/Lab21_9_countOccurrenceOfWords.java
+++ mysol_ed10/chap_21/lab21_9_countOccurrenceOfWords/src/lab21_9_countoccurrenceofwords/Lab21_9_countOccurrenceOfWords.java
@@ -22,26 +22,34 @@ public class Lab21_9_countOccurrenceOfWords {
         // TODO code application logic here
         String text = "Good morning. Have a good class. Have a good visit. Have fun!";
         
-        Map<String, Integer> map1 = new TreeMap<>();
+//        Map<String, Integer> map1 = new TreeMap<>();
         
         String[] words = text.split("[\\s+\\p{P}]");
+        
+        Map map1 = countOccurrenceOfWords(words);
+        System.out.println("Map: " + map1);
+        map1.forEach((k, v) -> System.out.println(k + "\t" + v));
+        
+    }
+    
+    public static Map<String, Integer> countOccurrenceOfWords(String[] words) {
+        Map<String, Integer> map2 = new TreeMap<>();
         
         for(int i = 0; i < words.length; i++) {
             String key = words[i].toLowerCase();
             
             if(key.length() > 0) {
-                if(!map1.containsKey(key)) {
-                    map1.put(key, 1);
+                if(!map2.containsKey(key)) {
+                    map2.put(key, 1);
                 } else {
-                    int value = map1.get(key);
+                    int value = map2.get(key);
                     value++;
-                    map1.put(key, value);
+                    map2.put(key, value);
                 }
             }
         }
         
-        map1.forEach((k, v) -> System.out.println(k + "\t" + v));
-        
+        return map2;
     }
     
 }
