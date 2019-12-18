5be600e424ecf1bc07fc8ea4fa7150b26f595f6e
specter01wj <specter01wj@gmail.com>
2019-12-18 15:13:42 -0500

    11/24/2019 v1

---
+++ mysol_ed10/chap_21/lab21_8/src/lab21_8/Lab21_8.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab21_8;
+
+import java.util.*;
+
+/**
+ *
+ * @author james.wang
+ */
+
+// TestMap
+public class Lab21_8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Map<String, Integer> hashMap1 = new HashMap<>();
+        hashMap1.put("Smith", 30);
+        hashMap1.put("Anderson", 31);
+        hashMap1.put("Lewis", 29);
+        hashMap1.put("Cook", 29);
+        System.out.println("Display entries in HashMap");
+        System.out.println(hashMap1 + "\n");
+    }
+    
+}
