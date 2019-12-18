2d07f5321cb8756a69ee999d3f3414b8071173b2
specter01wj <specter01wj@gmail.com>
2019-12-18 12:44:06 -0500

    11/24/2019 v1

--- mysol_ed10/chap_21/lab21_7_countKeywords/src/lab21_7_countkeywords/Lab21_7_countKeywords.java
+++ mysol_ed10/chap_21/lab21_7_countKeywords/src/lab21_7_countkeywords/Lab21_7_countKeywords.java
@@ -5,6 +5,9 @@
  */
 package lab21_7_countkeywords;
 
+import java.util.*;
+import java.io.*;
+
 /**
  *
  * @author james.wang
@@ -18,6 +21,33 @@ public class Lab21_7_countKeywords {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public static int countKeywords(File file) throws Exception {
+        String[] keywordString = {"abstract", "assert", "boolean", 
+            "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "enum",
+            "extends", "for", "final", "finally", "float", "goto",
+            "if", "implements", "import", "instanceof", "int", 
+            "interface", "long", "native", "new", "package", "private",
+            "protected", "public", "return", "short", "static", 
+            "strictfp", "super", "switch", "synchronized", "this",
+            "throw", "throws", "transient", "try", "void", "volatile",
+            "while", "true", "false", "null"};
+        
+        Set<String> keywordSet = new HashSet<>(Arrays.asList(keywordString));
+        int count = 0;
+        
+        Scanner input = new Scanner(file);
+        
+        while(input.hasNext()) {
+            String word = input.next();
+            if(keywordSet.contains(word)) {
+                count++;
+            }
+        }
+        
+        return count;
     }
     
 }
