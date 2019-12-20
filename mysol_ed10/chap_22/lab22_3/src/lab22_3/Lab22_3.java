52c3677ad9b69a87e60b1d40420497c65c8f31f1
specter01wj <specter01wj@gmail.com>
2019-12-19 20:11:11 -0500

    12/19/2019 v1

--- mysol_ed10/chap_22/lab22_3/src/lab22_3/Lab22_3.java
+++ mysol_ed10/chap_22/lab22_3/src/lab22_3/Lab22_3.java
@@ -27,8 +27,25 @@ public class Lab22_3 {
         System.out.print("Enter second integer: ");
         int n = input.nextInt();
 
-        System.out.println("The greatest common divisor for " + m +
-          " and " + n + " is " + gcd(m, n));
+        System.out.println("The greatest common divisor for " + 
+                m + " and " + n + " is " + gcd(m, n));
+    }
+    
+    public static int gcd(int m, int n) {
+        int gcd = 1;
+        
+        if(m % n == 0) {
+            return n;
+        }
+        
+        for(int k = n / 2; k >= 1; k--) {
+            if(m % k == 0 && n % k == 0) {
+                gcd = k;
+                break;
+            }
+        }
+        
+        return gcd;
     }
     
 }
