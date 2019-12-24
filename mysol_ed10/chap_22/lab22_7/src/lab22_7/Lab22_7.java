64a335c910ade555b6efb39e0e3f9093a63d206a
specter01wj <specter01wj@gmail.com>
2019-12-24 17:51:03 -0500

    12/24/2019 v1

--- mysol_ed10/chap_22/lab22_7/src/lab22_7/Lab22_7.java
+++ mysol_ed10/chap_22/lab22_7/src/lab22_7/Lab22_7.java
@@ -18,6 +18,40 @@ public class Lab22_7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Find all prime numbers <= n, enter n: ");
+        int n = input.nextInt();
+
+        boolean[] primes = new boolean[n + 1]; // Prime number sieve
+
+        // Initialize primes[i] to true
+        for (int i = 0; i < primes.length; i++) {
+          primes[i] = true; 
+        }
+
+        for (int k = 2; k <= n / k; k++) {
+          if (primes[k]) {
+            for (int i = k; i <= n / k; i++) {
+              primes[k * i] = false; // k * i is not prime
+            }
+          }
+        }
+
+        final int NUMBER_PER_LINE = 10; // Display 10 per line
+        int count = 0; // Count the number of prime numbers found so far
+        // Print prime numbers
+        for (int i = 2; i < primes.length; i++) {
+          if (primes[i]) {
+            count++;
+            if (count % 10 == 0) 
+              System.out.printf("%7d\n", i);
+            else
+              System.out.printf("%7d", i);          
+          }
+        }
+
+        System.out.println("\n" + count + 
+          " prime(s) less than or equal to " + n);
     }
     
 }
