49d243325578dbb90e6ff3497e09d9961479a019
specter01wj <specter01wj@gmail.com>
2019-12-25 14:08:48 -0500

    12/25/2019 v1

--- mysol_ed10/chap_22/lab22_9_eightQueens/src/lab22_9_eightqueens/Lab22_9_eightQueens.java
+++ mysol_ed10/chap_22/lab22_9_eightQueens/src/lab22_9_eightqueens/Lab22_9_eightQueens.java
@@ -44,7 +44,7 @@ public class Lab22_9_eightQueens extends Application {
             }
 
         // Display queens
-        Image image = new Image("image/queen_2.png");
+        Image image = new Image("image/queen_e5.jpg");
         for (int i = 0; i < SIZE; i++) {
             labels[i][queens[i]].setGraphic(new ImageView(image));
         }
@@ -58,46 +58,51 @@ public class Lab22_9_eightQueens extends Application {
 
     /** Search for a solution */
     private boolean search() {
-      // k - 1 indicates the number of queens placed so far
-      // We are looking for a position in the kth row to place a queen
-      int k = 0;
-      while (k >= 0 && k < SIZE) {
-        // Find a position to place a queen in the kth row
-        int j = findPosition(k);
-        if (j < 0) {
-          queens[k] = -1;
-          k--; // back track to the previous row
-        } else {
-          queens[k] = j;
-          k++;
+        // k - 1 indicates the number of queens placed so far
+        // We are looking for a position in the kth row to place a queen
+        int k = 0;
+        while (k >= 0 && k < SIZE) {
+            // Find a position to place a queen in the kth row
+            int j = findPosition(k);
+            if (j < 0) {
+                queens[k] = -1;
+                k--; // back track to the previous row
+            } else {
+                queens[k] = j;
+                k++;
+            }
         }
-      }
 
-      if (k == -1)
-        return false; // No solution
-      else
-        return true; // A solution is found
+        if (k == -1) {
+            return false; // No solution
+        } else {
+            return true; // A solution is found
+        }
     }
 
     public int findPosition(int k) {
-      int start = queens[k] + 1; // Search for a new placement
+        int start = queens[k] + 1; // Search for a new placement
 
-      for (int j = start; j < SIZE; j++) {
-        if (isValid(k, j))
-          return j; // (k, j) is the place to put the queen now
-      }
+        for (int j = start; j < SIZE; j++) {
+            if (isValid(k, j)) {
+                return j; // (k, j) is the place to put the queen now
+            }
+        }
 
-      return -1;
+        return -1;
     }
 
     /** Return true if a queen can be placed at (row, column) */
     public boolean isValid(int row, int column) {
-      for (int i = 1; i <= row; i++)
-        if (queens[row - i] == column // Check column
-          || queens[row - i] == column - i // Check upleft diagonal
-          || queens[row - i] == column + i) // Check upright diagonal
-          return false; // There is a conflict
-      return true; // No conflict
+        for (int i = 1; i <= row; i++) {
+            if (queens[row - i] == column // Check column
+              || queens[row - i] == column - i // Check upleft diagonal
+              || queens[row - i] == column + i) { // Check upright diagonal
+                return false; // There is a conflict
+            }
+        }
+        
+        return true; // No conflict
     }
     /**
      * @param args the command line arguments
