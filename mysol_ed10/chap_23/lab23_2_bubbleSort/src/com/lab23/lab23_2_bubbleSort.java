426663e93d7329ca1e0eeb61bc0bc222d1ca326f
Jin Wang <specter01wj@gmail.com>
2021-04-08 23:51:55 -0400

    02/12/2021 v1

--- mysol_ed10/chap_23/lab23_2_bubbleSort/src/com/lab23/lab23_2_bubbleSort.java
+++ mysol_ed10/chap_23/lab23_2_bubbleSort/src/com/lab23/lab23_2_bubbleSort.java
@@ -1,4 +1,14 @@
 package com.lab23;
+import java.util.*;
 
 public class lab23_2_bubbleSort {
+
+    public static void main(String[] args) {
+        int[] input = {7,1,5,3,6,4};//{7,6,4,3,1};
+        int output = maxProfit(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+    }
+
+
+
 }
