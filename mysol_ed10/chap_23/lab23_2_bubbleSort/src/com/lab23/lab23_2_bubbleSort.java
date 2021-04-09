83ed25e6f1afe0acbab95390529081de7a2e6086
Jin Wang <specter01wj@gmail.com>
2021-04-09 00:16:15 -0400

    02/12/2021 v1

--- mysol_ed10/chap_23/lab23_2_bubbleSort/src/com/lab23/lab23_2_bubbleSort.java
+++ mysol_ed10/chap_23/lab23_2_bubbleSort/src/com/lab23/lab23_2_bubbleSort.java
@@ -5,14 +5,13 @@ public class lab23_2_bubbleSort {
 
     public static void main(String[] args) {
         int[] input = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
-        System.out.println("input: " + Arrays.toString(input));
         int[] output = bubbleSort(input);
-        System.out.println("output: " + Arrays.toString(output));
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
     }
 
     public static int[] bubbleSort(int[] list) {
         boolean needNextPass = true;
-        int[] output = list;
+        int[] output = list.clone();
 
         for(int k = 1; k < output.length && needNextPass; k++) {
             needNextPass = false;
