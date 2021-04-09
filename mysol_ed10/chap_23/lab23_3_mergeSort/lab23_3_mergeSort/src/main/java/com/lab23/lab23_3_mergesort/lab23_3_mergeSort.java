b3621a335a513ccbffacbaeb3e252cbb80ada4cd
Jin Wang <specter01wj@gmail.com>
2021-04-09 00:23:32 -0400

    02/12/2021 v1

---
+++ mysol_ed10/chap_23/lab23_3_mergeSort/lab23_3_mergeSort/src/main/java/com/lab23/lab23_3_mergesort/lab23_3_mergeSort.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab23.lab23_3_mergesort;
+import java.util.*;
+
+/**
+ *
+ * @author jinwang
+ */
+public class lab23_3_mergeSort {
+    
+    public static void main(String[] args) {
+        int[] input = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        int[] output = mergeSort(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
+    }
+    
+    public static int[] mergeSort(int[] list) {
+        
+        
+        
+    }
+    
+}
