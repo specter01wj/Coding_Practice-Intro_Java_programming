246aec25b1f1faaa27c135d2324473d97aaae3ed
Jin Wang <specter01wj@gmail.com>
2021-04-09 11:08:44 -0400

    02/12/2021 v1

--- mysol_ed10/chap_23/lab23_3_mergeSort/lab23_3_mergeSort/src/main/java/com/lab23/lab23_3_mergesort/lab23_3_mergeSort.java
+++ mysol_ed10/chap_23/lab23_3_mergeSort/lab23_3_mergeSort/src/main/java/com/lab23/lab23_3_mergesort/lab23_3_mergeSort.java
@@ -20,8 +20,38 @@ public class lab23_3_mergeSort {
     
     public static int[] mergeSort(int[] list) {
         
+        if(list.length > 1) {
+            int[] firstHalf = new int[list.length / 2];
+            System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+            mergeSort(firstHalf);
+            
+            int[] secondHalf = new int[list.length - list.length / 2];
+            System.arraycopy(list, list.length / 2,
+                secondHalf, 0, list.length - list.length / 2);
+            mergeSort(secondHalf);
+            
+            merge(firstHalf, secondHalf, list);
+        }
         
-        
+    }
+    
+    public static void merge(int[] list1, int[] list2, int[] temp) {
+        int current1 = 0; // Current index in list1
+        int current2 = 0; // Current index in list2
+        int current3 = 0; // Current index in temp
+
+        while (current1 < list1.length && current2 < list2.length) {
+          if (list1[current1] < list2[current2])
+            temp[current3++] = list1[current1++];
+          else
+            temp[current3++] = list2[current2++];
+        }
+
+        while (current1 < list1.length)
+          temp[current3++] = list1[current1++];
+
+        while (current2 < list2.length)
+          temp[current3++] = list2[current2++];
     }
     
 }
