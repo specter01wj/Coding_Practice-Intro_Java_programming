58ef23d4a19f6ecf7d88504a335c1792612e4b3a
Jin Wang <specter01wj@gmail.com>
2021-04-08 23:57:12 -0400

    02/12/2021 v1

--- mysol_ed10/chap_23/lab23_2_bubbleSort/src/com/lab23/lab23_2_bubbleSort.java
+++ mysol_ed10/chap_23/lab23_2_bubbleSort/src/com/lab23/lab23_2_bubbleSort.java
@@ -7,6 +7,7 @@ public class lab23_2_bubbleSort {
         int[] input = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
         System.out.println("input: " + Arrays.toString(input));
 
+
         
         int output = bubbleSort(input);
         System.out.println("output: " + (output));
