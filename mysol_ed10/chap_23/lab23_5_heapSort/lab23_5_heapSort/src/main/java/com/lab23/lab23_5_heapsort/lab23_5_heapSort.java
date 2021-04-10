776953c114f273277e4de836d8073b6bbb9d052c
Jin Wang <specter01wj@gmail.com>
2021-04-10 17:38:05 -0400

    02/12/2021 v1

---
+++ mysol_ed10/chap_23/lab23_5_heapSort/lab23_5_heapSort/src/main/java/com/lab23/lab23_5_heapsort/lab23_5_heapSort.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab23.lab23_5_heapsort;
+import java.util.*;
+
+/**
+ *
+ * @author jinwang
+ */
+public class lab23_5_heapSort {
+    
+    public static void main(String[] args) {
+        int[] input = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        System.out.println("input: " + Arrays.toString(input));
+        heapSort(input);
+        System.out.println("output: " + Arrays.toString(input));
+    }
+    
+    public static <E extends Comparable> void heapSort(E[] list) {
+        // Create a Heap of integers
+        Heap<E> heap = new Heap<E>();
+
+        // Add elements to the heap
+        for (int i = 0; i < list.length; i++)
+          heap.add(list[i]);
+
+        // Remove elements from the heap
+        for (int i = list.length - 1; i >= 0; i--) {
+          list[i] = heap.remove();
+        }
+    }
+    
+}
