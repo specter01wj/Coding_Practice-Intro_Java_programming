6e5ad06486852ea3573ab62e5108a4fd4849c836
Jin Wang <specter01wj@gmail.com>
2021-04-18 23:58:03 -0400

    04/17/2021 v1

--- mysol_ed10/chap_24/lab24_2_linkedList/src/main/java/com/lab23/lab24_2_linkedlist/lab24_2_linkedlist.java
+++ mysol_ed10/chap_24/lab24_2_linkedList/src/main/java/com/lab23/lab24_2_linkedlist/lab24_2_linkedlist.java
@@ -60,6 +60,7 @@ public class lab24_2_linkedlist {
         
         
         
+        
         System.out.println("\nAfter clearing the list, the list size is " 
           + list.size());
       }
