8fedf32c43a9e47e7efc078681537f085bc9358e
Jin Wang <specter01wj@gmail.com>
2021-04-18 23:53:01 -0400

    04/17/2021 v1

---
+++ mysol_ed10/chap_24/lab24_2_linkedList/src/main/java/com/lab23/lab24_2_linkedlist/MyLinkedList.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab23.lab24_2_linkedlist;
+
+/**
+ *
+ * @author jinwang
+ */
+public class MyLinkedList<E> extends MyAbstractList<E>  {
+    
+}
