73769c807090d2712bd6a696e0d6b191f605e7f5
Jin Wang <specter01wj@gmail.com>
2026-01-03 01:56:22 -0500

    v5

--- mysol_ed10/chap_25/lab25_1_testBST/src/main/java/com/jinwang/lab25_1_testbst/Lab25_1_testBST.java
+++ mysol_ed10/chap_25/lab25_1_testBST/src/main/java/com/jinwang/lab25_1_testbst/Lab25_1_testBST.java
@@ -1,6 +1,7 @@
 
 package com.jinwang.lab25_1_testbst;
 
+import java.util.ArrayList;
 /**
  *
  * @author jinwang
@@ -8,6 +9,44 @@ package com.jinwang.lab25_1_testbst;
 public class Lab25_1_testBST {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // ---------- String BST ----------
+        BST<String> tree = new BST<>();
+
+        tree.add("George");
+        tree.add("Michael");
+        tree.add("Tom");
+        tree.add("Adam");
+        tree.add("Jones");
+        tree.add("Peter");
+        tree.add("Daniel");
+
+        // Traversals
+        System.out.print("Inorder (sorted): ");
+        tree.inorder();
+
+        System.out.print("\nPostorder: ");
+        tree.postorder();
+
+        System.out.print("\nPreorder: ");
+        tree.preorder();
+
+        System.out.println("\nNumber of nodes: " + tree.size());
+
+        // Search
+        System.out.println("Contains Peter? " + tree.contains("Peter"));
+
+        // Path from root to Peter
+        System.out.print("Path from root to Peter: ");
+        ArrayList<BST.TreeNode<String>> path = tree.path("Peter");
+        for (BST.TreeNode<String> node : path) {
+          System.out.print(node.element + " ");
+        }
+
+        // ---------- Integer BST ----------
+        Integer[] numbers = { 2, 4, 3, 1, 8, 5, 6, 7 };
+        BST<Integer> intTree = new BST<>(numbers);
+
+        System.out.print("\nInorder (sorted): ");
+        intTree.inorder();
     }
 }
