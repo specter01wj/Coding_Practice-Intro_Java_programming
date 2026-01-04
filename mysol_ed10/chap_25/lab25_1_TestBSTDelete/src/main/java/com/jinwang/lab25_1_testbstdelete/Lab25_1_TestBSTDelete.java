8f2bb5eb5576f002e1bfd7a8fb4ae70afb551a1a
Jin Wang <specter01wj@gmail.com>
2026-01-03 23:59:09 -0500

    v1

---
+++ mysol_ed10/chap_25/lab25_1_TestBSTDelete/src/main/java/com/jinwang/lab25_1_testbstdelete/Lab25_1_TestBSTDelete.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.lab25_1_testbstdelete;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Lab25_1_TestBSTDelete {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
