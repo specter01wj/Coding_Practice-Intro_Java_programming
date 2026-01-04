397448859e2aa94538dc0d67ffda5e23f698bee3
Jin Wang <specter01wj@gmail.com>
2026-01-04 02:09:43 -0500

    v10

---
+++ mysol_ed10/chap_25/lab25_4_TestBSTWithIterator/src/main/java/com/jinwang/lab25_4_testbstwithiterator/Tree.java
@@ -0,0 +1,50 @@
+
+package com.jinwang.lab25_3_bstanimation;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author jinwang
+ */
+public interface Tree<E> extends Iterable<E> {
+
+  /* ---------- Query Operations ---------- */
+
+  /** Returns true if the specified element exists in the tree */
+  boolean contains(E element);
+
+  /** Returns the number of elements in the tree */
+  int size();
+
+  /** Returns true if the tree contains no elements */
+  default boolean isEmpty() {
+    return size() == 0;
+  }
+
+  /* ---------- Modification Operations ---------- */
+
+  /** Inserts the specified element into the tree
+   *  @return true if the element was inserted successfully */
+  boolean add(E element);
+
+  /** Removes the specified element from the tree
+   *  @return true if the element was removed successfully */
+  boolean remove(E element);
+
+  /* ---------- Traversal Operations ---------- */
+
+  /** Performs an inorder traversal */
+  void inorder();
+
+  /** Performs a preorder traversal */
+  void preorder();
+
+  /** Performs a postorder traversal */
+  void postorder();
+
+  /* ---------- Iteration ---------- */
+
+  @Override
+  Iterator<E> iterator();
+}
