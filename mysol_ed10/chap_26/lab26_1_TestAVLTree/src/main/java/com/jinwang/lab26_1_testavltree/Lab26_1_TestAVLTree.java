ed07055bb71d1fb79b301dd9b6c1cc32bd64ed38
Jin Wang <specter01wj@gmail.com>
2026-01-04 12:53:10 -0500

    v1

---
+++ mysol_ed10/chap_26/lab26_1_TestAVLTree/src/main/java/com/jinwang/lab26_1_testavltree/Lab26_1_TestAVLTree.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.lab26_1_testavltree;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Lab26_1_TestAVLTree {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
