f39c700664f91403b9806319e7b51661db80d0e9
Jin Wang <specter01wj@gmail.com>
2026-01-04 00:52:01 -0500

    v1

---
+++ mysol_ed10/chap_25/lab25_3_BSTAnimation/src/main/java/com/jinwang/lab25_3_bstanimation/Lab25_3_BSTAnimation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.lab25_3_bstanimation;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Lab25_3_BSTAnimation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
