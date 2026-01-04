92b7df6a7d1fa0416f6e84cf2ae2cfab9a2eb146
Jin Wang <specter01wj@gmail.com>
2026-01-04 00:01:47 -0500

    v5

--- mysol_ed10/chap_25/lab25_1_TestBSTDelete/src/main/java/com/jinwang/lab25_1_testbstdelete/Lab25_1_TestBSTDelete.java
+++ mysol_ed10/chap_25/lab25_1_TestBSTDelete/src/main/java/com/jinwang/lab25_1_testbstdelete/Lab25_1_TestBSTDelete.java
@@ -8,6 +8,43 @@ package com.jinwang.lab25_1_testbstdelete;
 public class Lab25_1_TestBSTDelete {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BST<String> tree = new BST<>();
+
+        tree.add("George");
+        tree.add("Michael");
+        tree.add("Tom");
+        tree.add("Adam");
+        tree.add("Jones");
+        tree.add("Peter");
+        tree.add("Daniel");
+
+        System.out.println("Initial tree:");
+        printTree(tree);
+
+        System.out.println("\nAfter removing George:");
+        tree.remove("George");
+        printTree(tree);
+
+        System.out.println("\nAfter removing Adam:");
+        tree.remove("Adam");
+        printTree(tree);
+
+        System.out.println("\nAfter removing Michael:");
+        tree.remove("Michael");
+        printTree(tree);
     }
+    
+    private static <E extends Comparable<E>> void printTree(BST<E> tree) {
+        System.out.print("Inorder (sorted): ");
+        tree.inorder();
+
+        System.out.print("\nPostorder: ");
+        tree.postorder();
+
+        System.out.print("\nPreorder: ");
+        tree.preorder();
+
+        System.out.println("\nNumber of nodes: " + tree.size());
+    }
+    
 }
