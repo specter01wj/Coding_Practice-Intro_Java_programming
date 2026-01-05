e71be8e6774f457e814cdfc71aae648dd6115a0f
Jin Wang <specter01wj@gmail.com>
2026-01-04 21:33:46 -0500

    v2

--- mysol_ed10/chap_27/lab27_1_TestMyHashMap/src/main/java/com/jinwang/lab27_1_testmyhashmap/Lab27_1_TestMyHashMap.java
+++ mysol_ed10/chap_27/lab27_1_TestMyHashMap/src/main/java/com/jinwang/lab27_1_testmyhashmap/Lab27_1_TestMyHashMap.java
@@ -1,16 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jinwang.lab27_1_testmyhashmap;
 
-/**
- *
- * @author jinwang
- */
+
 public class Lab27_1_TestMyHashMap {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MyMap<String, Integer> map = new MyHashMap<>();
+
+        map.put("Smith", 30);
+        map.put("Anderson", 31);
+        map.put("Lewis", 29);
+        map.put("Cook", 29);
+        map.put("Smith", 65); // overwrite
+
+        System.out.println("Initial map entries:");
+        System.out.println(map);
+
+        System.out.println("\nGet value by key:");
+        System.out.println("Lewis -> " + map.get("Lewis"));
+
+        System.out.println("\nKey / Value checks:");
+        System.out.println("Contains key 'Smith'? " + map.containsKey("Smith"));
+        System.out.println("Contains value 33? " + map.containsValue(33));
+
+        map.remove("Smith");
+        System.out.println("\nAfter removing 'Smith':");
+        System.out.println(map);
+
+        map.clear();
+        System.out.println("\nAfter clear():");
+        System.out.println(map);
     }
 }
