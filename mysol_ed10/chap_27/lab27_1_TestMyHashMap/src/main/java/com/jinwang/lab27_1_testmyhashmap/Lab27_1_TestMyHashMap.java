0579466650c3151aa1ee9fa35daf3fc909987fd6
Jin Wang <specter01wj@gmail.com>
2026-01-04 21:24:44 -0500

    v1

---
+++ mysol_ed10/chap_27/lab27_1_TestMyHashMap/src/main/java/com/jinwang/lab27_1_testmyhashmap/Lab27_1_TestMyHashMap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.lab27_1_testmyhashmap;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Lab27_1_TestMyHashMap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
