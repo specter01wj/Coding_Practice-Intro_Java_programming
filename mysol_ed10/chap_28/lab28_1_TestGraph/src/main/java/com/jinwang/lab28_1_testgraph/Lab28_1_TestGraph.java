e4430f1cf96d97f33560df0fe2e5f76254f44c76
Jin Wang <specter01wj@gmail.com>
2026-01-06 17:50:21 -0500

    v1

---
+++ mysol_ed10/chap_28/lab28_1_TestGraph/src/main/java/com/jinwang/lab28_1_testgraph/Lab28_1_TestGraph.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab28_1_testgraph;
+
+
+public class Lab28_1_TestGraph {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
