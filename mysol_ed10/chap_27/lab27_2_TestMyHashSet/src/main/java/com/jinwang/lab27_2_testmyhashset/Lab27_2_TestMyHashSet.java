b98198787f09fd351b506cf9a06a39c7a152f15d
Jin Wang <specter01wj@gmail.com>
2026-01-06 02:03:49 -0500

    v3

--- mysol_ed10/chap_27/lab27_2_TestMyHashSet/src/main/java/com/jinwang/lab27_2_testmyhashset/Lab27_2_TestMyHashSet.java
+++ mysol_ed10/chap_27/lab27_2_TestMyHashSet/src/main/java/com/jinwang/lab27_2_testmyhashset/Lab27_2_TestMyHashSet.java
@@ -1,16 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jinwang.lab27_2_testmyhashset;
 
-/**
- *
- * @author jinwang
- */
+
 public class Lab27_2_TestMyHashSet {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         MySet<String> set = new MyHashSet<>();
+
+        set.add("Smith");
+        set.add("Anderson");
+        set.add("Lewis");
+        set.add("Cook");
+        set.add("Smith"); // duplicate
+
+        System.out.println("Elements in set: " + set);
+        System.out.println("Number of elements: " + set.size());
+        System.out.println("Contains Smith? " + set.contains("Smith"));
+
+        set.remove("Smith");
+
+        System.out.print("Names in uppercase: ");
+        for (String s : set) {
+          System.out.print(s.toUpperCase() + " ");
+        }
+
+        set.clear();
+        System.out.println("\nElements in set: " + set);
     }
 }
