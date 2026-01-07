f42ffd561626efe30b8d0ba0e0e861f231b6ba97
Jin Wang <specter01wj@gmail.com>
2026-01-07 00:55:24 -0500

    v1

---
+++ mysol_ed10/chap_28/lab28_2_DisplayUSMap/src/main/java/com/jinwang/lab28_2_displayusmap/Lab28_2_DisplayUSMap.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab28_2_displayusmap;
+
+
+public class Lab28_2_DisplayUSMap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
