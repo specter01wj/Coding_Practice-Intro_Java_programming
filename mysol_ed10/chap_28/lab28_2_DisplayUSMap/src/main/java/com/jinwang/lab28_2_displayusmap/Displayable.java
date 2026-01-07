97795db66c8f52fb4b07e4327d3d281776fa282e
Jin Wang <specter01wj@gmail.com>
2026-01-07 01:21:07 -0500

    v4

---
+++ mysol_ed10/chap_28/lab28_2_DisplayUSMap/src/main/java/com/jinwang/lab28_2_displayusmap/Displayable.java
@@ -0,0 +1,15 @@
+
+package com.jinwang.lab28_2_displayusmap;
+
+
+public interface Displayable {
+
+  /** Return x-coordinate of the vertex */
+  int getX();
+
+  /** Return y-coordinate of the vertex */
+  int getY();
+
+  /** Return display name of the vertex */
+  String getName();
+}
