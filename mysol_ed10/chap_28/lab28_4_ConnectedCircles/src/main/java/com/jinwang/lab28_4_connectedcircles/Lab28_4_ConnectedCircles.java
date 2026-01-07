e244d3c29ba4ef3e7102b211bf593b5e5af081e0
Jin Wang <specter01wj@gmail.com>
2026-01-07 02:27:58 -0500

    v1

---
+++ mysol_ed10/chap_28/lab28_4_ConnectedCircles/src/main/java/com/jinwang/lab28_4_connectedcircles/Lab28_4_ConnectedCircles.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab28_4_connectedcircles;
+
+
+public class Lab28_4_ConnectedCircles {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
