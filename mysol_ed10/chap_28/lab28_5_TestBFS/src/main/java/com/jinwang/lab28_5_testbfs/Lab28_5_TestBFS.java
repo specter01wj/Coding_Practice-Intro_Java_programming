6c4233075bf7aeefb9786ca4b46b806dcec688b6
Jin Wang <specter01wj@gmail.com>
2026-01-07 02:57:54 -0500

    v5

---
+++ mysol_ed10/chap_28/lab28_5_TestBFS/src/main/java/com/jinwang/lab28_5_testbfs/Lab28_5_TestBFS.java
@@ -0,0 +1,56 @@
+
+package com.jinwang.lab28_5_testbfs;
+
+import java.util.List;
+
+public class Lab28_5_TestBFS {
+
+    public static void main(String[] args) {
+        String[] vertices = {
+          "Seattle", "San Francisco", "Los Angeles",
+          "Denver", "Kansas City", "Chicago", "Boston",
+          "New York", "Atlanta", "Miami", "Dallas", "Houston"
+        };
+
+        int[][] edges = {
+          {0, 1}, {0, 3}, {0, 5},
+          {1, 0}, {1, 2}, {1, 3},
+          {2, 1}, {2, 3}, {2, 4}, {2, 10},
+          {3, 0}, {3, 1}, {3, 2}, {3, 4}, {3, 5},
+          {4, 2}, {4, 3}, {4, 5}, {4, 7}, {4, 8}, {4, 10},
+          {5, 0}, {5, 3}, {5, 4}, {5, 6}, {5, 7},
+          {6, 5}, {6, 7},
+          {7, 4}, {7, 5}, {7, 6}, {7, 8},
+          {8, 4}, {8, 7}, {8, 9}, {8, 10}, {8, 11},
+          {9, 8}, {9, 11},
+          {10, 2}, {10, 4}, {10, 8}, {10, 11},
+          {11, 8}, {11, 9}, {11, 10}
+        };
+
+        Graph<String> graph = new UnweightedGraph<>(vertices, edges);
+
+        AbstractGraph<String>.Tree bfsTree =
+            graph.bfs(graph.getIndex("Chicago"));
+
+        List<Integer> searchOrder = bfsTree.getSearchOrder();
+
+        System.out.println(
+            bfsTree.getNumberOfVerticesFound() +
+            " vertices are searched in this BFS order:"
+        );
+
+        for (int index : searchOrder) {
+          System.out.println(graph.getVertex(index));
+        }
+
+        System.out.println();
+        for (int i = 0; i < graph.getSize(); i++) {
+          if (bfsTree.getParent(i) != -1) {
+            System.out.println(
+              "parent of " + graph.getVertex(i) +
+              " is " + graph.getVertex(bfsTree.getParent(i))
+            );
+          }
+        }
+    }
+}
