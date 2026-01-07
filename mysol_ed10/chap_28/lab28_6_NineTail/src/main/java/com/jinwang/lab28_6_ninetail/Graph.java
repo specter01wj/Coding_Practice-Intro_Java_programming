0fd7d61d6a8ba1466c5780e29709dad706d3c5c7
Jin Wang <specter01wj@gmail.com>
2026-01-07 03:09:54 -0500

    v9

---
+++ mysol_ed10/chap_28/lab28_6_NineTail/src/main/java/com/jinwang/lab28_6_ninetail/Graph.java
@@ -0,0 +1,31 @@
+
+package com.jinwang.lab28_6_ninetail;
+
+import java.util.List;
+
+public interface Graph<V> {
+
+  int getSize();
+
+  List<V> getVertices();
+
+  V getVertex(int index);
+
+  int getIndex(V v);
+
+  List<Integer> getNeighbors(int index);
+
+  int getDegree(int v);
+
+  void printEdges();
+
+  void clear();
+
+  boolean addVertex(V vertex);
+
+  boolean addEdge(int u, int v);
+
+  AbstractGraph<V>.Tree dfs(int v);
+
+  AbstractGraph<V>.Tree bfs(int v);
+}
---
+++ mysol_ed10/chap_28/lab28_6_NineTail/src/main/java/com/jinwang/lab28_6_ninetail/UnweightedGraph.java
@@ -0,0 +1,23 @@
+
+package com.jinwang.lab28_6_ninetail;
+
+import java.util.List;
+
+public class UnweightedGraph<V> extends AbstractGraph<V> {
+
+  /** Construct an empty graph */
+  public UnweightedGraph() {
+    super();
+  }
+
+  /** Construct a graph from vertex array and edge array */
+  public UnweightedGraph(V[] vertices, int[][] edges) {
+    super(vertices, edges);
+  }
+
+  /** Construct a graph from vertex list and edge list */
+  public UnweightedGraph(List<V> vertices, List<Edge> edges) {
+    super(vertices, edges);
+  }
+
+}
