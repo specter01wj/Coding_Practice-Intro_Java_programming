3da07404786675c2559be8940bf3aad03ac5edaf
Jin Wang <specter01wj@gmail.com>
2026-01-08 03:01:25 -0500

    v1

---
+++ mysol_ed10/chap_29/lab29_4_WeightedNineTail/src/main/java/com/jinwang/lab29_4_weightedninetail/Lab29_4_WeightedNineTail.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab29_4_weightedninetail;
+
+
+public class Lab29_4_WeightedNineTail {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
