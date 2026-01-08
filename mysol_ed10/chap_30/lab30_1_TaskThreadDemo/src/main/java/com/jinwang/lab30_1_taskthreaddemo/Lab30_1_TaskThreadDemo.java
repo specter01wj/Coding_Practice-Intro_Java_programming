dbc636a9bbf5609d4e720be511332e4326001a11
Jin Wang <specter01wj@gmail.com>
2026-01-08 13:54:06 -0500

    v1

---
+++ mysol_ed10/chap_30/lab30_1_TaskThreadDemo/src/main/java/com/jinwang/lab30_1_taskthreaddemo/Lab30_1_TaskThreadDemo.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_1_taskthreaddemo;
+
+
+public class Lab30_1_TaskThreadDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
