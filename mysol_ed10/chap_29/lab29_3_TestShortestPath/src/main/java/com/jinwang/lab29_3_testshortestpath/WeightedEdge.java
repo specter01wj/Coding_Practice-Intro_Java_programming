204ced7e5331bb1151ad3632b99fab758ee5cdde
Jin Wang <specter01wj@gmail.com>
2026-01-08 02:54:49 -0500

    v3

---
+++ mysol_ed10/chap_29/lab29_3_TestShortestPath/src/main/java/com/jinwang/lab29_3_testshortestpath/WeightedEdge.java
@@ -0,0 +1,19 @@
+
+package com.jinwang.lab29_2_testminimumspanningtree;
+
+
+public class WeightedEdge extends AbstractGraph.Edge
+    implements Comparable<WeightedEdge> {
+
+  public double weight;
+
+  public WeightedEdge(int u, int v, double weight) {
+    super(u, v);
+    this.weight = weight;
+  }
+
+  @Override
+  public int compareTo(WeightedEdge edge) {
+    return Double.compare(this.weight, edge.weight);
+  }
+}
