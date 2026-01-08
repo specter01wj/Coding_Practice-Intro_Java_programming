8035bcc652376e9b6dbc55d476f6938b71653add
Jin Wang <specter01wj@gmail.com>
2026-01-08 18:17:08 -0500

    v1

---
+++ mysol_ed10/chap_30/lab30_3_ExecutorDemo/src/main/java/com/jinwang/lab30_3_executordemo/Lab30_3_ExecutorDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.lab30_3_executordemo;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Lab30_3_ExecutorDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
