86b7755a04c73cf820a7a202b8b8406def59bf66
Jin Wang <specter01wj@gmail.com>
2026-01-08 18:33:06 -0500

    v3

---
+++ mysol_ed10/chap_30/lab30_4_AccountWithoutSync/src/main/java/com/jinwang/lab30_4_accountwithoutsync/Lab30_4_AccountWithoutSync.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_4_accountwithoutsync;
+
+
+public class Lab30_4_AccountWithoutSync {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
