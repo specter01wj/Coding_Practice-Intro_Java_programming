eef594573ab4c61781ecc6d423040dd1eaa5fafa
Jin Wang <specter01wj@gmail.com>
2026-01-08 15:53:12 -0500

    v3

--- mysol_ed10/chap_30/lab30_2_FlashText/src/main/java/com/jinwang/lab30_2_flashtext/Lab30_2_FlashText.java
+++ mysol_ed10/chap_30/lab30_2_FlashText/src/main/java/com/jinwang/lab30_2_flashtext/Lab30_2_FlashText.java
@@ -1,10 +1,46 @@
 
 package com.jinwang.lab30_2_flashtext;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Lab30_2_FlashText {
+public class Lab30_2_FlashText extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+      Label lblText = new Label("Programming is fun");
+      StackPane pane = new StackPane(lblText);
+
+      Thread thread = new Thread(() -> {
+        try {
+          while (true) {
+            Platform.runLater(() -> {
+              if (lblText.getText().isEmpty()) {
+                lblText.setText("Welcome");
+              } else {
+                lblText.setText("");
+              }
+            });
+            Thread.sleep(200);
+          }
+        } catch (InterruptedException ignored) {
+        }
+      });
+
+      thread.setDaemon(true);
+      thread.start();
+
+      Scene scene = new Scene(pane, 200, 50);
+      primaryStage.setTitle("FlashText");
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 }
