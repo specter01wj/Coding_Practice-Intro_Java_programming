953346df288fe5a9ff20c1e9166591ce0d40467e
Jin Wang <specter01wj@gmail.com>
2026-01-08 02:53:42 -0500

    v1

---
+++ mysol_ed10/chap_29/lab29_3_TestShortestPath/src/main/java/com/jinwang/lab29_3_testshortestpath/Lab29_3_TestShortestPath.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab29_3_testshortestpath;
+
+
+public class Lab29_3_TestShortestPath {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
