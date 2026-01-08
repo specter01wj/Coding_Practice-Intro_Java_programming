52cc543e7ca3d45efd2e48646b3b4aa237a85a87
Jin Wang <specter01wj@gmail.com>
2026-01-08 03:03:07 -0500

    v3

--- mysol_ed10/chap_29/lab29_4_WeightedNineTail/src/main/java/com/jinwang/lab29_4_weightedninetail/Lab29_4_WeightedNineTail.java
+++ mysol_ed10/chap_29/lab29_4_WeightedNineTail/src/main/java/com/jinwang/lab29_4_weightedninetail/Lab29_4_WeightedNineTail.java
@@ -5,6 +5,22 @@ package com.jinwang.lab29_4_weightedninetail;
 public class Lab29_4_WeightedNineTail {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Enter an initial nine coins' Hs and Ts: ");
+        Scanner input = new Scanner(System.in);
+        char[] initialNode = input.nextLine().toCharArray();
+
+        WeightedNineTailModel model = new WeightedNineTailModel();
+
+        var path =
+            model.getShortestPath(NineTailModel.getIndex(initialNode));
+
+        System.out.println("The steps to flip the coins are ");
+        for (int p : path) {
+          NineTailModel.printNode(NineTailModel.getNode(p));
+        }
+
+        System.out.println("The number of flips is " +
+            model.getNumberOfFlips(
+                NineTailModel.getIndex(initialNode)));
     }
 }
