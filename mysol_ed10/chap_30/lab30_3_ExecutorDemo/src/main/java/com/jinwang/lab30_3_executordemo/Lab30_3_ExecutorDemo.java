65d4bf9358cec554bcc1ca51e195211a44cb3f7d
Jin Wang <specter01wj@gmail.com>
2026-01-08 18:17:56 -0500

    v2

--- mysol_ed10/chap_30/lab30_3_ExecutorDemo/src/main/java/com/jinwang/lab30_3_executordemo/Lab30_3_ExecutorDemo.java
+++ mysol_ed10/chap_30/lab30_3_ExecutorDemo/src/main/java/com/jinwang/lab30_3_executordemo/Lab30_3_ExecutorDemo.java
@@ -1,16 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jinwang.lab30_3_executordemo;
 
-/**
- *
- * @author jinwang
- */
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Lab30_3_ExecutorDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Create a fixed thread pool with three threads
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        // Submit tasks
+        executor.execute(new PrintChar('a', 100));
+        executor.execute(new PrintChar('b', 100));
+        executor.execute(new PrintNum(100));
+
+        // Initiate orderly shutdown
+        executor.shutdown();
     }
+}
+
+// Task for printing a character multiple times
+class PrintChar implements Runnable {
+  private final char charToPrint;
+  private final int times;
+
+  public PrintChar(char c, int t) {
+    this.charToPrint = c;
+    this.times = t;
+  }
+
+  @Override
+  public void run() {
+    for (int i = 0; i < times; i++) {
+      System.out.print(charToPrint);
+    }
+  }
+}
+
+// Task for printing numbers from 1 to n
+class PrintNum implements Runnable {
+  private final int lastNum;
+
+  public PrintNum(int n) {
+    this.lastNum = n;
+  }
+
+  @Override
+  public void run() {
+    for (int i = 1; i <= lastNum; i++) {
+      System.out.print(" " + i);
+    }
+  }
 }
