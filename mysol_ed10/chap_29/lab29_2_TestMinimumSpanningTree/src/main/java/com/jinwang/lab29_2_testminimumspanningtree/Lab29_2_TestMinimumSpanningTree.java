ac66dcbec23b05230ebc4a1f2de60f2c0624019b
Jin Wang <specter01wj@gmail.com>
2026-01-08 02:15:59 -0500

    v1

---
+++ mysol_ed10/chap_29/lab29_2_TestMinimumSpanningTree/src/main/java/com/jinwang/lab29_2_testminimumspanningtree/Lab29_2_TestMinimumSpanningTree.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab29_2_testminimumspanningtree;
+
+
+public class Lab29_2_TestMinimumSpanningTree {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
