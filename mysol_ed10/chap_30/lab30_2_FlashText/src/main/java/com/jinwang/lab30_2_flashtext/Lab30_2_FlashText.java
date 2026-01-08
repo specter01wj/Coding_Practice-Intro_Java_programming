abbd87421217f83629add9401f663986a3f9a76a
Jin Wang <specter01wj@gmail.com>
2026-01-08 15:47:30 -0500

    v3

---
+++ mysol_ed10/chap_30/lab30_2_FlashText/src/main/java/com/jinwang/lab30_2_flashtext/Lab30_2_FlashText.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_2_flashtext;
+
+
+public class Lab30_2_FlashText {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
