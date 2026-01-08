3fda060f60c3433150f9809af58a7af5973d4ded
Jin Wang <specter01wj@gmail.com>
2026-01-08 18:40:56 -0500

    v5

---
+++ mysol_ed10/chap_30/lab30_5_AccountWithSyncUsingLock/src/main/java/com/jinwang/lab30_5_accountwithsyncusinglock/Lab30_5_AccountWithSyncUsingLock.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_5_accountwithsyncusinglock;
+
+
+public class Lab30_5_AccountWithSyncUsingLock {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
