91f8ac706457ea12943ab5131370cac9036d0a57
Jin Wang <specter01wj@gmail.com>
2026-01-08 18:45:20 -0500

    v8

--- mysol_ed10/chap_30/lab30_5_AccountWithSyncUsingLock/src/main/java/com/jinwang/lab30_5_accountwithsyncusinglock/Lab30_5_AccountWithSyncUsingLock.java
+++ mysol_ed10/chap_30/lab30_5_AccountWithSyncUsingLock/src/main/java/com/jinwang/lab30_5_accountwithsyncusinglock/Lab30_5_AccountWithSyncUsingLock.java
@@ -1,10 +1,56 @@
 
 package com.jinwang.lab30_5_accountwithsyncusinglock;
 
+import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
 
 public class Lab30_5_AccountWithSyncUsingLock {
+    
+    private static final Account account = new Account();
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 100; i++) {
+          executor.execute(new AddAPennyTask());
+        }
+
+        executor.shutdown();
+
+        while (!executor.isTerminated()) {
+        }
+
+        System.out.println("What is balance? " + account.getBalance());
+    }
+    
+    // Task class
+    public static class AddAPennyTask implements Runnable {
+      @Override
+      public void run() {
+        account.deposit(1);
+      }
+    }
+
+    // Account class protected by Lock
+    public static class Account {
+      private static final Lock lock = new ReentrantLock();
+      private int balance = 0;
+
+      public int getBalance() {
+        return balance;
+      }
+
+      public void deposit(int amount) {
+        lock.lock();
+        try {
+          int newBalance = balance + amount;
+          Thread.sleep(5); // simulate delay
+          balance = newBalance;
+        } catch (InterruptedException ex) {
+          Thread.currentThread().interrupt();
+        } finally {
+          lock.unlock();
+        }
+      }
     }
 }
