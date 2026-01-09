9ffc759cb8ac552e169a7be912d846515a09e478
Jin Wang <specter01wj@gmail.com>
2026-01-09 03:45:43 -0500

    v7

---
+++ mysol_ed10/chap_30/lab30_8_ConsumerProducerUsingBlockingQueue/src/main/java/com/jinwang/lab30_8_consumerproducerusingblockingqueue/Lab30_8_ConsumerProducerUsingBlockingQueue.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_8_consumerproducerusingblockingqueue;
+
+
+public class Lab30_8_ConsumerProducerUsingBlockingQueue {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
