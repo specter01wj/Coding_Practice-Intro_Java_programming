8dc89fe7b05ae1fee551da44527b370639e10bd3
Jin Wang <specter01wj@gmail.com>
2026-01-09 03:48:31 -0500

    v8

--- mysol_ed10/chap_30/lab30_8_ConsumerProducerUsingBlockingQueue/src/main/java/com/jinwang/lab30_8_consumerproducerusingblockingqueue/Lab30_8_ConsumerProducerUsingBlockingQueue.java
+++ mysol_ed10/chap_30/lab30_8_ConsumerProducerUsingBlockingQueue/src/main/java/com/jinwang/lab30_8_consumerproducerusingblockingqueue/Lab30_8_ConsumerProducerUsingBlockingQueue.java
@@ -1,10 +1,49 @@
 
 package com.jinwang.lab30_8_consumerproducerusingblockingqueue;
 
+import java.util.concurrent.*;
 
 public class Lab30_8_ConsumerProducerUsingBlockingQueue {
-
+    
+    private static final ArrayBlockingQueue<Integer> buffer =
+      new ArrayBlockingQueue<>(2);
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(new ProducerTask());
+        executor.execute(new ConsumerTask());
+        executor.shutdown();
+    }
+    
+    // Producer task
+    private static class ProducerTask implements Runnable {
+      @Override
+      public void run() {
+        try {
+          int i = 1;
+          while (true) {
+            System.out.println("Producer writes " + i);
+            buffer.put(i++); // blocks automatically if full
+            Thread.sleep((int)(Math.random() * 10000));
+          }
+        } catch (InterruptedException ex) {
+          Thread.currentThread().interrupt();
+        }
+      }
+    }
+
+    // Consumer task
+    private static class ConsumerTask implements Runnable {
+      @Override
+      public void run() {
+        try {
+          while (true) {
+            System.out.println("\t\t\tConsumer reads " + buffer.take());
+            Thread.sleep((int)(Math.random() * 10000));
+          }
+        } catch (InterruptedException ex) {
+          Thread.currentThread().interrupt();
+        }
+      }
     }
 }
