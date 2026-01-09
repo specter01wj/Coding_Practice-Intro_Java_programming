bb6d1cc68746ff4238d6d8fc2ba20625a62274e8
Jin Wang <specter01wj@gmail.com>
2026-01-09 04:20:41 -0500

    v13

---
+++ mysol_ed10/chap_30/lab30_11_ParallelMax/src/main/java/com/jinwang/lab30_11_parallelmax/Lab30_11_ParallelMax.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_11_parallelmax;
+
+
+public class Lab30_11_ParallelMax {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
