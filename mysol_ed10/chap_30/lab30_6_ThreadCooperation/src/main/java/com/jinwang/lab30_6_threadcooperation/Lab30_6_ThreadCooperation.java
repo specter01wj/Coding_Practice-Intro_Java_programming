b6874011d094fb181de78d26079e31671fda6d25
Jin Wang <specter01wj@gmail.com>
2026-01-09 03:22:29 -0500

    v1

---
+++ mysol_ed10/chap_30/lab30_6_ThreadCooperation/src/main/java/com/jinwang/lab30_6_threadcooperation/Lab30_6_ThreadCooperation.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_6_threadcooperation;
+
+
+public class Lab30_6_ThreadCooperation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
