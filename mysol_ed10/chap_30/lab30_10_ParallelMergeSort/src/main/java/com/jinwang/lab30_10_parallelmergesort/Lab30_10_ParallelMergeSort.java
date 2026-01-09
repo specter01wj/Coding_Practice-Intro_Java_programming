61682c2c45cbe36a10fcb3ee3f874b29a9b040d4
Jin Wang <specter01wj@gmail.com>
2026-01-09 04:14:08 -0500

    v11

---
+++ mysol_ed10/chap_30/lab30_10_ParallelMergeSort/src/main/java/com/jinwang/lab30_10_parallelmergesort/Lab30_10_ParallelMergeSort.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_10_parallelmergesort;
+
+
+public class Lab30_10_ParallelMergeSort {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
