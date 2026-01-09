1d9a05193213a410a354281369c2f4013b9eda02
Jin Wang <specter01wj@gmail.com>
2026-01-09 03:35:14 -0500

    v4

---
+++ mysol_ed10/chap_30/lab30_7_ConsumerProducer/src/main/java/com/jinwang/lab30_7_consumerproducer/Lab30_7_ConsumerProducer.java
@@ -0,0 +1,10 @@
+
+package com.jinwang.lab30_7_consumerproducer;
+
+
+public class Lab30_7_ConsumerProducer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
